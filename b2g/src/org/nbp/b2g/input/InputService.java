ede9efdbadb068a121b65f7bb750b85b54d29c35
Dave Mielke <dave@mielke.cc>
2015-02-07 20:48:57 -0500

    Add the basic methods to the input service object. (dm)

--- b2g/src/org/nbp/b2g/input/InputService.java
+++ b2g/src/org/nbp/b2g/input/InputService.java
@@ -1,7 +1,64 @@
 package org.nbp.b2g.input;
 
-import android.inputmethodservice.InputMethodService;
+import android.util.Log;
 import android.os.Bundle;
 
+import android.inputmethodservice.InputMethodService;
+import android.view.inputmethod.EditorInfo;
+import android.view.KeyEvent;
+
 public class InputService extends InputMethodService {
+  private static final String LOG_TAG = InputService.class.getName();
+
+  private static volatile InputService inputService = null;
+
+  public static InputService getInputService () {
+    return inputService;
+  }
+
+  @Override
+  public void onCreate () {
+    super.onCreate();
+
+    Log.d(LOG_TAG, "input service started");
+    inputService = this;
+  }
+
+  @Override
+  public void onDestroy () {
+    super.onDestroy();
+
+    Log.d(LOG_TAG, "input service stopped");
+    inputService = null;
+  }
+
+  @Override
+  public void onBindInput () {
+    Log.d(LOG_TAG, "input service bound");
+  }
+
+  @Override
+  public void onUnbindInput () {
+    Log.d(LOG_TAG, "input service unbound");
+  }
+
+  @Override
+  public void onStartInput (EditorInfo info, boolean restarting) {
+    Log.d(LOG_TAG, "input service " + (restarting? "reconnected": "connected"));
+  }
+
+  @Override
+  public void onFinishInput () {
+    Log.d(LOG_TAG, "input service disconnected");
+  }
+
+  @Override
+  public boolean onKeyDown (int code, KeyEvent event) {
+    return super.onKeyDown(code, event);
+  }
+
+  @Override
+  public boolean onKeyUp (int code, KeyEvent event) {
+    return super.onKeyUp(code, event);
+  }
 }
