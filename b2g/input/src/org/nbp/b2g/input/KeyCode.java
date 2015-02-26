daa601e82437d2d6a25f2b5e446abf3b95720fef
Dave Mielke <dave@mielke.cc>
2015-02-26 01:35:33 -0500

    Name the key codes after the proper dot numbers. (dm)

--- b2g/input/src/org/nbp/b2g/input/KeyCode.java
+++ b2g/input/src/org/nbp/b2g/input/KeyCode.java
@@ -13,15 +13,15 @@ public class KeyCode {
     return code;
   }
 
-  public final static int Dot1    = 769; //  getKeyCode("KEYCODE_BRL_DOT1");
-  public final static int Dot2    = 770; //  getKeyCode("KEYCODE_BRL_DOT2");
-  public final static int Dot3    = 771; //  getKeyCode("KEYCODE_BRL_DOT3");
-  public final static int Dot4    = 772; //  getKeyCode("KEYCODE_BRL_DOT4");
-  public final static int Dot5    = 773; //  getKeyCode("KEYCODE_BRL_DOT5");
-  public final static int Dot6    = 774; //  getKeyCode("KEYCODE_BRL_DOT6");
-  public final static int Dot7    = 775; //  getKeyCode("KEYCODE_BRL_DOT7");
+  public final static int Dot7    = 769; //  getKeyCode("KEYCODE_BRL_DOT1");
+  public final static int Dot3    = 770; //  getKeyCode("KEYCODE_BRL_DOT2");
+  public final static int Dot2    = 771; //  getKeyCode("KEYCODE_BRL_DOT3");
+  public final static int Dot1    = 772; //  getKeyCode("KEYCODE_BRL_DOT4");
+  public final static int Dot4    = 773; //  getKeyCode("KEYCODE_BRL_DOT5");
+  public final static int Dot5    = 774; //  getKeyCode("KEYCODE_BRL_DOT6");
+  public final static int Dot6    = 775; //  getKeyCode("KEYCODE_BRL_DOT7");
   public final static int Dot8    = 776; //  getKeyCode("KEYCODE_BRL_DOT8");
-  public final static int Dot9    = 777; //  getKeyCode("KEYCODE_BRL_DOT9");
+  public final static int Space   = 777; //  getKeyCode("KEYCODE_BRL_DOT9");
 
   public final static int Cursor1 = 778; //  getKeyCode("KEYCODE_BRL_CURSOR0");
   public final static int Cursor2 = 798; //  getKeyCode("KEYCODE_BRL_CURSOR20");
@@ -30,15 +30,15 @@ public class KeyCode {
   public final static int Forward = 819; //  getKeyCode("KEYCODE_BRL_FORWARD");
 
   public static int toKeyMask (int code) {
-    if (code == KeyCode.Dot4)    return KeyMask.Dot1;
-    if (code == KeyCode.Dot3)    return KeyMask.Dot2;
-    if (code == KeyCode.Dot2)    return KeyMask.Dot3;
-    if (code == KeyCode.Dot5)    return KeyMask.Dot4;
-    if (code == KeyCode.Dot6)    return KeyMask.Dot5;
-    if (code == KeyCode.Dot7)    return KeyMask.Dot6;
-    if (code == KeyCode.Dot1)    return KeyMask.Dot7;
+    if (code == KeyCode.Dot1)    return KeyMask.Dot1;
+    if (code == KeyCode.Dot2)    return KeyMask.Dot2;
+    if (code == KeyCode.Dot3)    return KeyMask.Dot3;
+    if (code == KeyCode.Dot4)    return KeyMask.Dot4;
+    if (code == KeyCode.Dot5)    return KeyMask.Dot5;
+    if (code == KeyCode.Dot6)    return KeyMask.Dot6;
+    if (code == KeyCode.Dot7)    return KeyMask.Dot7;
     if (code == KeyCode.Dot8)    return KeyMask.Dot8;
-    if (code == KeyCode.Dot9)    return KeyMask.Space;
+    if (code == KeyCode.Space)   return KeyMask.Space;
 
     if (code == KeyCode.Forward) return KeyMask.Forward;
     if (code == KeyCode.Back)    return KeyMask.Back;
