3a2409d18cc57f5e6227e877c23d58cea0d26590
Dave Mielke <dave@mielke.cc>
2015-02-27 06:53:58 -0500

    Log sent key events. (dm)

--- b2g/input/src/org/nbp/b2g/input/KeyAction.java
+++ b2g/input/src/org/nbp/b2g/input/KeyAction.java
@@ -7,6 +7,10 @@ import android.view.KeyEvent;
 public final class KeyAction extends Action {
   protected final int keyCode;
 
+  public void logKeyEventSent (int code, boolean press) {
+    inputService.logKeyEvent(code, press, "sent");
+  }
+
   @Override
   public final boolean performAction () {
     InputConnection connection = inputService.getCurrentInputConnection();
@@ -15,9 +19,11 @@ public final class KeyAction extends Action {
       KeyEvent event = new KeyEvent(KeyEvent.ACTION_DOWN, keyCode);
 
       if (connection.sendKeyEvent(event)) {
+        logKeyEventSent(keyCode, true);
         event = KeyEvent.changeAction(event, KeyEvent.ACTION_UP);
 
         if (connection.sendKeyEvent(event)) {
+          logKeyEventSent(keyCode, false);
           return true;
         }
       }
