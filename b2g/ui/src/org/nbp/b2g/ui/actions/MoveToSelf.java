0d1483ca7509a172c76f5865dc105c3e60315ac8
Dave Mielke <dave@mielke.cc>
2015-03-29 12:38:37 -0400

    Add the MoveToSelf action. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/actions/MoveToSelf.java
+++ b2g/ui/src/org/nbp/b2g/ui/actions/MoveToSelf.java
@@ -3,27 +3,21 @@ import org.nbp.b2g.ui.*;
 
 import android.view.accessibility.AccessibilityNodeInfo;
 
-public class MoveToParent extends ScreenAction {
+public class MoveToSelf extends ScreenAction {
   @Override
   public boolean performAction () {
     boolean moved = false;
     AccessibilityNodeInfo node = getCurrentNode();
 
     if (node != null) {
-      AccessibilityNodeInfo parent = node.getParent();
-
-      if (parent != null) {
-        if (setCurrentNode(parent, true)) moved = true;
-        parent.recycle();
-      }
-
+      if (setCurrentNode(node, true)) moved = true;
       node.recycle();
     }
 
     return moved;
   }
 
-  public MoveToParent () {
+  public MoveToSelf () {
     super();
   }
 }
