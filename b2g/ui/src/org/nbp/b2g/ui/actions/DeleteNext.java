6df4fc87319b28c040836622e64a90119d95aa8f
Dave Mielke <dave@mielke.cc>
2015-03-30 13:32:56 -0400

    Add a better way to delete characters. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/actions/DeleteNext.java
+++ b2g/ui/src/org/nbp/b2g/ui/actions/DeleteNext.java
@@ -1,9 +1,27 @@
 package org.nbp.b2g.ui.actions;
 import org.nbp.b2g.ui.*;
 
+import android.view.inputmethod.InputConnection;
 import android.view.KeyEvent;
 
 public class DeleteNext extends ScanCodeAction {
+  @Override
+  public boolean performAction () {
+    InputService service = getInputService();
+
+    if (service != null) {
+      InputConnection connection = service.getCurrentInputConnection();
+
+      if (connection != null) {
+        if (connection.deleteSurroundingText(0, 1)) {
+          return true;
+        }
+      }
+    }
+
+    return super.performAction();
+  }
+
   @Override
   protected String getScanCode () {
     return "DELETE";
--- b2g/ui/src/org/nbp/b2g/ui/actions/DeletePrevious.java
+++ b2g/ui/src/org/nbp/b2g/ui/actions/DeletePrevious.java
@@ -1,9 +1,27 @@
 package org.nbp.b2g.ui.actions;
 import org.nbp.b2g.ui.*;
 
+import android.view.inputmethod.InputConnection;
 import android.view.KeyEvent;
 
 public class DeletePrevious extends ScanCodeAction {
+  @Override
+  public boolean performAction () {
+    InputService service = getInputService();
+
+    if (service != null) {
+      InputConnection connection = service.getCurrentInputConnection();
+
+      if (connection != null) {
+        if (connection.deleteSurroundingText(1, 0)) {
+          return true;
+        }
+      }
+    }
+
+    return super.performAction();
+  }
+
   @Override
   protected String getScanCode () {
     return "BACKSPACE";
