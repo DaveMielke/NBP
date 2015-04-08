ac3121e570f20b9b921ca03fc4d59c7fbe956f44
Dave Mielke <dave@mielke.cc>
2015-04-08 18:57:41 -0400

    Implement more of the Bluetooth braille display interface. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/bluetooth/BrailleDisplay.java
+++ b2g/ui/src/org/nbp/b2g/ui/bluetooth/BrailleDisplay.java
@@ -2,6 +2,10 @@ package org.nbp.b2g.ui.bluetooth;
 import org.nbp.b2g.ui.*;
 
 import java.io.IOException;
+import java.io.Closeable;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import java.util.UUID;
 
 import android.util.Log;
@@ -13,9 +17,90 @@ import android.bluetooth.BluetoothSocket;
 public class BrailleDisplay extends Thread {
   private final static String LOG_TAG = BrailleDisplay.class.getName();
 
-  protected final UUID SERIAL_PROFILE_UUID = UUID.fromString(
+  private OutputStream outputStream = null;
+
+  private final static UUID SERIAL_PROFILE_UUID = UUID.fromString(
     "00001101-0000-1000-8000-00805F9B34FB"
   );
+
+  private static void closeObject (Closeable object, String description) {
+    try {
+      object.close();
+    } catch (IOException exception) {
+      Log.w(LOG_TAG, String.format("%s close error: %s", description, exception.getMessage()), exception);
+    }
+  }
+
+  private static BluetoothServerSocket getServerSocket (BluetoothAdapter adapter) {
+    try {
+      String name = ApplicationParameters.BLUETOOTH_SERVICE_NAME;
+      UUID uuid = SERIAL_PROFILE_UUID;
+
+      if (ApplicationParameters.BLUETOOTH_SECURE_CONNECTION) {
+        return adapter.listenUsingRfcommWithServiceRecord(name, uuid);
+      } else {
+        return adapter.listenUsingInsecureRfcommWithServiceRecord(name, uuid);
+      }
+    } catch (IOException exception) {
+      Log.w(LOG_TAG, "bluetooth server socket creation error: " + exception.getMessage());
+    }
+
+    return null;
+  }
+
+  private static BluetoothSocket getSessionSocket (BluetoothServerSocket server) {
+    try {
+      return server.accept();
+    } catch (IOException exception) {
+      Log.w(LOG_TAG, "bluetooth sdession socket creation error: " + exception.getMessage());
+    }
+
+    return null;
+  }
+
+  private static InputStream getInputStream (BluetoothSocket socket) {
+    try {
+      return socket.getInputStream();
+    } catch (IOException exception) {
+      Log.w(LOG_TAG, "bluetooth input stream creation error: " + exception.getMessage());
+    }
+
+    return null;
+  }
+
+  private static OutputStream getOutputStream (BluetoothSocket socket) {
+    try {
+      return socket.getOutputStream();
+    } catch (IOException exception) {
+      Log.w(LOG_TAG, "bluetooth output stream creation error: " + exception.getMessage());
+    }
+
+    return null;
+  }
+
+  protected static void logIgnoredByte (byte b) {
+    Log.w(LOG_TAG, String.format("input byte ignored: 0X%02X", b));
+  }
+
+  protected void onByteReceived (byte b) {
+    logIgnoredByte(b);
+  }
+
+  private void processInput (InputStream stream) {
+    while (true) {
+      int b;
+
+      try {
+        b = stream.read();
+        if (b == -1) break;
+      } catch (IOException exception) {
+        Log.w(LOG_TAG, "bluetooth input error: " + exception.getMessage());
+        break;
+      }
+
+      onByteReceived((byte)b);
+    }
+  }
 
   @Override
   public void run () {
@@ -24,32 +109,38 @@ public class BrailleDisplay extends Thread {
 
     if (adapter != null) {
       while (true) {
-        BluetoothServerSocket server = null;
-        BluetoothSocket socket = null;
+        BluetoothServerSocket serverSocket = getServerSocket(adapter);
 
-        try {
-          String name = ApplicationParameters.BLUETOOTH_SERVICE_NAME;
-          UUID uuid = SERIAL_PROFILE_UUID;
-
-          if (ApplicationParameters.BLUETOOTH_SECURE_CONNECTION) {
-            server = adapter.listenUsingRfcommWithServiceRecord(name, uuid);
-          } else {
-            server = adapter.listenUsingInsecureRfcommWithServiceRecord(name, uuid);
-          }
-        } catch (IOException exception) {
-          Log.w(LOG_TAG, "bluetooth server socket not created: " + exception.getMessage());
-          ApplicationUtilities.sleep(ApplicationParameters.BLUETOOTH_RETRY_INTERVAL);
-          continue;
-        }
-
-        try {
+        if (serverSocket != null) {
           Log.d(LOG_TAG, "bluetooth server listening");
-          socket = server.accept();
-          server.close();
-          server = null;
-          Log.d(LOG_TAG, "bluetooth server connected");
-        } catch (IOException exception) {
-          Log.w(LOG_TAG, "bluetooth socket not created: " + exception.getMessage());
+          BluetoothSocket sessionSocket = getSessionSocket(serverSocket);
+
+          closeObject(serverSocket, "bluetooth server socket");
+          serverSocket = null;
+
+          if (sessionSocket != null) {
+            Log.d(LOG_TAG, "bluetooth server connected");
+            InputStream inputStream = getInputStream(sessionSocket);
+
+            if (inputStream != null) {
+              synchronized (this) {
+                outputStream = getOutputStream(sessionSocket);
+              }
+
+              if (outputStream != null) {
+                processInput(inputStream);
+
+                synchronized (this) {
+                  outputStream = null;
+                }
+              }
+            }
+
+            closeObject(sessionSocket, "bluetooth session socket");
+            sessionSocket = null;
+          }
+        } else {
+          ApplicationUtilities.sleep(ApplicationParameters.BLUETOOTH_RETRY_INTERVAL);
         }
       }
     } else {
