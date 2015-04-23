5753b19c00911379e66c5379bc38a07d110ab438
Dave Mielke <dave@mielke.cc>
2015-04-23 13:58:41 -0400

    Make the enter key do a click when not on an editable field. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/host/actions/EnterKey.java
+++ b2g/ui/src/org/nbp/b2g/ui/host/actions/EnterKey.java
@@ -2,9 +2,23 @@ package org.nbp.b2g.ui.host.actions;
 import org.nbp.b2g.ui.host.*;
 import org.nbp.b2g.ui.*;
 
+import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.KeyEvent;
 
-public class EnterKey extends ScanCodeAction {
+public class EnterKey extends HostAction {
+  @Override
+  public boolean performAction () {
+    HostEndpoint endpoint = getHostEndpoint();
+
+    synchronized (endpoint) {
+      if (!endpoint.isEditable()) {
+        return performNodeAction(endpoint.getCurrentNode(), AccessibilityNodeInfo.ACTION_CLICK);
+      }
+    }
+
+    return super.performAction();
+  }
+
   @Override
   protected String getScanCode () {
     return "ENTER";
