03d0104aeb930e5844fa76c1dd31675194d02f2d
Dave Mielke <dave@mielke.cc>
2015-05-20 23:12:56 -0400

    Add the TouchEvents object. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/LanguageUtilities.java
+++ b2g/ui/src/org/nbp/b2g/ui/LanguageUtilities.java
@@ -1,6 +1,12 @@
 package org.nbp.b2g.ui;
 
-public class LanguageUtilities {
+import java.lang.reflect.*;
+
+import android.util.Log;
+
+public abstract class LanguageUtilities {
+  private final static String LOG_TAG = LanguageUtilities.class.getName();
+
   public static boolean canAssign (Class to, Class from) {
     return to.isAssignableFrom(from);
   }
@@ -30,6 +36,55 @@ public class LanguageUtilities {
 
   public static String getClassName (Class type) {
     return getClassName(type.getName());
+  }
+
+  public static Object invokeInstanceMethod (
+    Object instanceObject, String methodName,
+    Class[] argumentTypes, Object... argumentObjects
+  ) {
+    Class instanceClass = instanceObject.getClass();
+    String className = instanceClass.getName();
+    String methodReference = className + "." + methodName;
+
+    try {
+      Method method = instanceClass.getMethod(methodName, argumentTypes);
+      return method.invoke(instanceObject, argumentObjects);
+    } catch (NoSuchMethodException exception) {
+      Log.w(LOG_TAG, "cannot find method: " + methodReference);
+    } catch (IllegalAccessException exception) {
+      Log.w(LOG_TAG, "cannot access method: " + methodReference);
+    } catch (InvocationTargetException exception) {
+      Log.w(LOG_TAG, methodReference + " failed", exception.getCause());
+    }
+
+    return null;
+  }
+
+  public static Field getField (Class fieldClass, String fieldName) {
+    try {
+      return fieldClass.getField(fieldName);
+    } catch (NoSuchFieldException exception) {
+      String fieldReference = fieldClass.getName() + "." + fieldName;
+      Log.w(LOG_TAG, "cannot find field: " + fieldReference);
+    }
+
+    return null;
+  }
+
+  public static Integer getInstanceIntField (Object instanceObject, String fieldName) {
+    Class fieldClass = instanceObject.getClass();
+    Field field = getField(fieldClass, fieldName);
+
+    if (field != null) {
+      try {
+        return field.getInt(instanceObject);
+      } catch (IllegalAccessException exception) {
+        String fieldReference = fieldClass.getName() + "." + fieldName;
+        Log.w(LOG_TAG, "cannot access field: " + fieldReference);
+      }
+    }
+
+    return null;
   }
 
   private LanguageUtilities () {
---
+++ b2g/ui/src/org/nbp/b2g/ui/TouchEvents.java
@@ -0,0 +1,62 @@
+package org.nbp.b2g.ui;
+
+import android.util.Log;
+
+import android.content.Context;
+import android.hardware.input.InputManager;
+
+import android.view.InputDevice;
+import android.view.InputEvent;
+import android.view.MotionEvent;
+
+import android.os.SystemClock;
+
+public abstract class TouchEvents {
+  private final static String LOG_TAG = TouchEvents.class.getName();
+
+  private static InputManager getInputManager () {
+    Object service = ApplicationContext.getSystemService(Context.INPUT_SERVICE);
+    if (service != null) return (InputManager)service;
+
+    Log.w(LOG_TAG, "no input manager");
+    return null;
+  }
+
+  private static boolean injectEvent (InputEvent event) {
+    InputManager input = getInputManager();
+    if (input == null) return false;
+
+    Integer mode = LanguageUtilities.getInstanceIntField(input, "INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH");
+    if (mode == null) return false;
+
+    Boolean injected = (Boolean)LanguageUtilities.invokeInstanceMethod(
+      input, "injectInputEvent",
+      new Class[] {InputEvent.class, int.class},
+      event, mode
+    );
+
+    if (injected == null) return false;
+    return injected;
+  }
+
+  private static boolean injectEvent (int action, int x, int y) {
+    long now = SystemClock.uptimeMillis();
+    MotionEvent event = MotionEvent.obtain(now, now, action, x, y, 0);
+    event.setSource(InputDevice.SOURCE_TOUCHSCREEN);
+
+    boolean injected = injectEvent(event);
+    event.recycle();
+    return injected;
+  //InputManager.getInstance().injectInputEvent(event, InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH);
+  //getInputManager().injectInputEvent(event, true);
+  }
+
+  public static boolean tapScreen (int x, int y) {
+    if (!injectEvent(MotionEvent.ACTION_DOWN, x, y)) return false;
+    if (!injectEvent(MotionEvent.ACTION_UP, x, y)) return false;
+    return true;
+  }
+
+  private TouchEvents () {
+  }
+}
