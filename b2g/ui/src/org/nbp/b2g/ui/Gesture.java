ad2238df1867b2426fbdeac720c5e5aa2be0c195
Dave Mielke <dave@mielke.cc>
2015-05-24 12:37:34 -0400

    Improve the event timing for swipes. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/Gesture.java
+++ b2g/ui/src/org/nbp/b2g/ui/Gesture.java
@@ -1,16 +1,42 @@
 package org.nbp.b2g.ui;
 
+import android.util.Log;
+
 public abstract class Gesture {
+  private final static String LOG_TAG = Gesture.class.getName();
+
   private final static GestureInjector injector = Devices.pointer.get();
+
+  private static boolean begin (int x, int y) {
+    Log.v(LOG_TAG, String.format("gesture begin: %dx%d", x, y));
+
+    if (injector == null) return true;
+    return injector.gestureBegin(x, y);
+  }
+
+  private static boolean end () {
+    Log.v(LOG_TAG, "gesture end");
+
+    if (injector == null) return true;
+    return injector.gestureEnd();
+  }
+
+  private static boolean move (int x, int y) {
+    Log.v(LOG_TAG, String.format("gesture move: %dx%d", x, y));
+
+    if (injector == null) return true;
+    return injector.gestureMove(x, y);
+  }
+
 
   public static boolean tap (int x, int y, int count) {
     if (count < 1) return false;
     if (ApplicationContext.isTouchExplorationActive()) count += 1;
 
     while (true) {
-      if (!injector.gestureBegin(x, y)) return false;
+      if (!begin(x, y)) return false;
       ApplicationUtilities.sleep(45);
-      if (!injector.gestureEnd()) return false;
+      if (!end()) return false;
       if ((count -= 1) == 0) return true;
       ApplicationUtilities.sleep(100);
     }
@@ -29,28 +55,29 @@ public abstract class Gesture {
 
     double totalDistance = Math.hypot(horizontalDistance, verticalDistance);
     double stepDistance = 10.0;
-    double stepCount = totalDistance / stepDistance;
+    double stepCount = Math.rint(totalDistance / stepDistance);
 
     double horizontalStep = horizontalDistance / stepCount;
     double verticalStep = verticalDistance / stepCount;
 
-    if (!injector.gestureBegin(x1, y1)) return false;
+    if (!begin(x1, y1)) return false;
     int stepInterval = 10;
 
-    while (stepCount > 1.0) {
-      stepCount -= 1.0;
+    while (true) {
       ApplicationUtilities.sleep(stepInterval);
+      if (stepCount < 1.5) break;
+      stepCount -= 1.0;
 
       horizontalPosition += horizontalStep;
       verticalPosition += verticalStep;
 
-      int x = (int)Math.round(horizontalPosition);
-      int y = (int)Math.round(verticalPosition);
-      if (!injector.gestureMove(x, y)) return false;
+      int x = (int)Math.rint(horizontalPosition);
+      int y = (int)Math.rint(verticalPosition);
+      if (!move(x, y)) return false;
     }
 
-    if (!injector.gestureMove(x2, y2)) return false;
-    return injector.gestureEnd();
+    if (!move(x2, y2)) return false;
+    return end();
   }
 
   private Gesture () {
