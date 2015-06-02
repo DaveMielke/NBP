5d13dc409d8e2cf13db22718f6705718268d572a
Dave Mielke <dave@mielke.cc>
2015-06-02 14:19:54 -0400

    Add support for multi-finger swipes. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/MotionDevice.java
+++ b2g/ui/src/org/nbp/b2g/ui/MotionDevice.java
@@ -11,14 +11,60 @@ public class MotionDevice implements GestureInjector {
 
   private final Instrumentation instrumentation = new Instrumentation();
 
-  private long startTime;
-  private int lastX;
-  private int lastY;
+  private int pointerCount;
+  private MotionEvent.PointerProperties[] pointerProperties;
+  private MotionEvent.PointerCoords[] pointerCoordinates;
 
-  private void resetFields () {
+  private int deviceIdentifier;
+  private int metaState;
+  private int buttonState;
+  private float xPrecision;
+  private float yPrecision;
+  private int edgeFlags;
+  private int eventFlags;
+  private long startTime;
+
+  private void clearFields () {
+    pointerCount = 0;
+    pointerProperties = null;
+    pointerCoordinates = null;
+
+    deviceIdentifier = 0;
+    metaState = 0;
+    buttonState = 0;
+    xPrecision = 1.0f;
+    yPrecision = 1.0f;
+    edgeFlags = 0;
+    eventFlags = 0;
     startTime = 0;
-    lastX = 0;
-    lastY = 0;
+  }
+
+  private void setFields (int fingers, int x, int y) {
+    pointerProperties = new MotionEvent.PointerProperties[fingers];
+    pointerCoordinates = new MotionEvent.PointerCoords[fingers];
+
+    for (int index=0; index<fingers; index+=1) {
+      MotionEvent.PointerProperties properties = new MotionEvent.PointerProperties();
+      pointerProperties[index] = properties;
+
+      properties.id = index;
+    }
+
+    for (int index=0; index<fingers; index+=1) {
+      MotionEvent.PointerCoords coordinates = new MotionEvent.PointerCoords();
+      pointerCoordinates[index] = coordinates;
+
+      coordinates.x = (float)x;
+      coordinates.y = (float)y;
+      coordinates.pressure = 1.0f;
+      coordinates.size = 1.0f;
+
+      int increment = (index + 1) / 2;
+      if ((index % 2) == 1) increment = -increment;
+      coordinates.x += (float)increment;
+    }
+
+    startTime = ApplicationUtilities.getSystemClock();
   }
 
   private boolean injectEvent (MotionEvent event) {
@@ -26,20 +72,23 @@ public class MotionDevice implements GestureInjector {
     return true;
   }
 
-  private boolean injectEvent (int action, int x, int y) {
+  private boolean injectEvent (int action) {
     long now = ApplicationUtilities.getSystemClock();
-    MotionEvent event = MotionEvent.obtain(startTime, now, action, x, y, 0);
-    event.setSource(InputDevice.SOURCE_TOUCHSCREEN);
+
+    MotionEvent event = MotionEvent.obtain(
+      startTime, now, action,
+      pointerCount, pointerProperties, pointerCoordinates,
+      metaState, buttonState, xPrecision, yPrecision, deviceIdentifier,
+      edgeFlags, InputDevice.SOURCE_TOUCHSCREEN, eventFlags
+    );
 
     boolean injected = injectEvent(event);
     event.recycle();
-
-    if (injected) {
-      lastX = x;
-      lastY = y;
-    }
-
     return injected;
+  }
+
+  private int makePointerAction (int action) {
+    return action | ((pointerCount - 1) << MotionEvent.ACTION_POINTER_INDEX_SHIFT);
   }
 
   @Override
@@ -48,17 +97,37 @@ public class MotionDevice implements GestureInjector {
       "motion event: begin [%d,%d] Fingers:%d", x, y, fingers
     ));
 
-    startTime = ApplicationUtilities.getSystemClock();
-    return injectEvent(MotionEvent.ACTION_DOWN, x, y);
+    setFields(fingers, x, y);
+
+    pointerCount += 1;
+    if (!injectEvent(MotionEvent.ACTION_DOWN)) return false;
+
+    while (pointerCount < fingers) {
+      pointerCount += 1;
+      if (!injectEvent(makePointerAction(MotionEvent.ACTION_POINTER_DOWN))) return false;
+    }
+
+    return true;
   }
 
   @Override
   public boolean gestureEnd () {
     Log.v(LOG_TAG, "motion event: end");
-    boolean injected = injectEvent(MotionEvent.ACTION_UP, lastX, lastY);
 
-    resetFields();
-    return injected;
+    while (pointerCount > 1) {
+      if (!injectEvent(makePointerAction(MotionEvent.ACTION_POINTER_UP))) break;
+      pointerCount -= 1;
+    }
+
+    if (pointerCount == 1) {
+      if (injectEvent(MotionEvent.ACTION_UP)) {
+        pointerCount -= 1;
+      }
+    }
+
+    boolean ok = pointerCount == 0;
+    clearFields();
+    return ok;
   }
 
   @Override
@@ -67,10 +136,24 @@ public class MotionDevice implements GestureInjector {
       "motion event: move [%d,%d]", x, y
     ));
 
-    return injectEvent(MotionEvent.ACTION_MOVE, x, y);
+    float xIncrement;
+    float yIncrement;
+
+    {
+      MotionEvent.PointerCoords coordinates = pointerCoordinates[0];
+      xIncrement = (float)x - coordinates.x;
+      yIncrement = (float)y - coordinates.y;
+    }
+
+    for (MotionEvent.PointerCoords coordinates : pointerCoordinates) {
+      coordinates.x += xIncrement;
+      coordinates.y += yIncrement;
+    }
+
+    return injectEvent(MotionEvent.ACTION_MOVE);
   }
 
   public MotionDevice () {
-    resetFields();
+    clearFields();
   }
 }
