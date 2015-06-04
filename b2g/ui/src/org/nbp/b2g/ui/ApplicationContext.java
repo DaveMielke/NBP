bce9d730c51aec32ee6e0fc2a57347b40e1334e1
Dave Mielke <dave@mielke.cc>
2015-06-04 04:52:55 -0400

    Don't crash if insufficient privilege when trying to start the screen monitor. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/ApplicationContext.java
+++ b2g/ui/src/org/nbp/b2g/ui/ApplicationContext.java
@@ -82,9 +82,9 @@ public abstract class ApplicationContext {
     return result == PackageManager.PERMISSION_GRANTED;
   }
 
-  public static void enableService (Class<? extends AccessibilityService> serviceClass) {
+  public static boolean enableService (Class<? extends AccessibilityService> serviceClass) {
     Context context = getContext();
-    if (context == null) return;
+    if (context == null) return false;
 
     Intent intent = new Intent(context, serviceClass);
     ComponentName component = intent.getComponent();
@@ -99,21 +99,28 @@ public abstract class ApplicationContext {
     for (String service : services.split(":")) {
       if (service.startsWith(packagePrefix)) {
         Log.d(LOG_TAG, "accessibility service already enabled: " + serviceClass.getName());
-        return;
+        return true;
       }
     }
 
-    Log.i(LOG_TAG, "enabling accessibility service: " + serviceClass.getName());
     String serviceString = packagePrefix + className;
-
     if (services.length() == 0) {
       services = serviceString;
     } else {
       services += ":" + serviceString;
     }
 
-    Settings.Secure.putString(resolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, services);
-    Settings.Secure.putString(resolver, Settings.Secure.ACCESSIBILITY_ENABLED, "1");
+    try {
+      Settings.Secure.putString(resolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, services);
+      Settings.Secure.putString(resolver, Settings.Secure.ACCESSIBILITY_ENABLED, "1");
+
+      Log.i(LOG_TAG, "accessibility service enabled: " + serviceClass.getName());
+      return true;
+    } catch (SecurityException exception) {
+      Log.w(LOG_TAG, "can't enable accessibility service: " + serviceClass.getName());
+    }
+
+    return false;
   }
 
   public static Object getSystemService (String name) {
