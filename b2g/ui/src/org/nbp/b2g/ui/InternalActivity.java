fc04bc9562c36580feed5791a63e7d09743de314
Dave Mielke <dave@mielke.cc>
2015-06-26 00:29:09 -0400

    Add createHorizontalScrollView(). (dm)

--- b2g/ui/src/org/nbp/b2g/ui/InternalActivity.java
+++ b2g/ui/src/org/nbp/b2g/ui/InternalActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ScrollView;
+import android.widget.HorizontalScrollView;
 
 public abstract class InternalActivity extends Activity {
   protected ViewGroup createVerticalScrollView () {
@@ -15,6 +16,17 @@ public abstract class InternalActivity extends Activity {
 
   protected ViewGroup createVerticalScrollView (View content) {
     ViewGroup view = createVerticalScrollView();
+    view.addView(content);
+    return view;
+  }
+
+  protected ViewGroup createHorizontalScrollView () {
+    HorizontalScrollView view = new HorizontalScrollView(this);
+    return view;
+  }
+
+  protected ViewGroup createHorizontalScrollView (View content) {
+    ViewGroup view = createHorizontalScrollView();
     view.addView(content);
     return view;
   }
