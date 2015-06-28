5a0df5ccd2e30c8dba865657d2de2d06b8c613e4
Dave Mielke <dave@mielke.cc>
2015-06-28 01:44:26 -0400

    Respond to keyboard-based navigation. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/ScreenMonitor.java
+++ b2g/ui/src/org/nbp/b2g/ui/ScreenMonitor.java
@@ -66,6 +66,73 @@ public class ScreenMonitor extends AccessibilityService {
     return false;
   }
 
+  private static String toString (Collection<CharSequence> lines) {
+    StringBuilder sb = new StringBuilder();
+
+    if (lines != null) {
+      for (CharSequence line : lines) {
+        if (line.length() == 0) continue;
+        if (sb.length() > 0) sb.append('\n');
+        sb.append(line);
+      }
+    }
+
+    if (sb.length() == 0) return null;
+    return sb.toString();
+  }
+
+  private static void showText (Collection<CharSequence> lines) {
+    String string = toString(lines);
+    if (string != null) Endpoints.setPopupEndpoint(string);
+  }
+
+  private static void appendProperty (StringBuilder sb, String label, String value) {
+    if (value != null) {
+      if (!value.isEmpty()) {
+        if (sb.length() > 0) sb.append('\n');
+        sb.append(label);
+        sb.append(": ");
+        sb.append(value);
+      }
+    }
+  }
+
+  private static void appendProperty (StringBuilder sb, String label, int value) {
+    if (value != -1) appendProperty(sb, label, Integer.toString(value));
+  }
+
+  private static void appendProperty (StringBuilder sb, String label, Collection<CharSequence> value) {
+    appendProperty(sb, label, toString(value));
+  }
+
+  private static void say (AccessibilityEvent event) {
+    StringBuilder sb = new StringBuilder();
+
+    {
+      String string = event.toString();
+      int index = string.indexOf(" TYPE_");
+
+      string = string.substring(index+1);
+      index = string.indexOf('_');
+
+      string = string.substring(index+1);
+      index = string.indexOf(' ');
+
+      string = string.substring(0, index).replace('_', ' ').toLowerCase();
+      appendProperty(sb, "Event", string);
+    }
+
+    appendProperty(sb, "Count", event.getItemCount());
+    appendProperty(sb, "Current", event.getCurrentItemIndex());
+    appendProperty(sb, "From", event.getFromIndex());
+    appendProperty(sb, "To", event.getToIndex());
+    appendProperty(sb, "Text", event.getText());
+
+    if (sb.length() > 0) {
+      Devices.speech.get().say(sb.toString());
+    }
+  }
+
   private void logMissingEventComponent (String component) {
     if (ApplicationSettings.LOG_UPDATES) {
       Log.d(LOG_TAG, "no accessibility event " + component);
@@ -81,15 +148,60 @@ public class ScreenMonitor extends AccessibilityService {
     }
   }
 
+  private static void setCurrentNode (AccessibilityEvent event) {
+    AccessibilityNodeInfo root = event.getSource();
+
+    if (root != null) {
+      ScreenUtilities.logNavigation(root, "set event root");
+
+      {
+        int childIndex = event.getCurrentItemIndex();
+
+        if (childIndex != -1) {
+          int from = event.getFromIndex();
+          if (from != -1) childIndex -= from;
+        }
+
+        if ((childIndex >= 0) && (childIndex < root.getChildCount())) {
+          AccessibilityNodeInfo child = root.getChild(childIndex);
+
+          if (child != null) {
+            root.recycle();
+            root = child;
+            ScreenUtilities.logNavigation(root, "set event child");
+          }
+        }
+      }
+
+      {
+        AccessibilityNodeInfo node = ScreenUtilities.findCurrentNode(root);
+
+        if (node != null) {
+          ScreenUtilities.logNavigation(node, "set event node");
+          ScreenUtilities.setCurrentNode(node);
+          node.recycle();
+        }
+      }
+
+      root.recycle();
+    }
+  }
+
   private static void handleViewSelected (AccessibilityEvent event, AccessibilityNodeInfo view) {
     if ((view == null) || ScreenUtilities.isSeekable(view)) {
       int count = event.getItemCount();
 
       if (count != -1) {
         int index = event.getCurrentItemIndex();
+        int percentage =
+          (count == 0)? 0:
+          (count == 1)? 100:
+          ((index * 100) / (count - 1));
 
-        ApplicationUtilities.message("%d%%", ((index * 100) / count));
+        ApplicationUtilities.message("%d%%", percentage);
       }
+    } else {
+      setCurrentNode(event);
     }
   }
 
@@ -106,26 +218,11 @@ public class ScreenMonitor extends AccessibilityService {
     }
   }
 
-  private static void showText (Collection<CharSequence> text) {
-    if (text != null) {
-      StringBuilder sb = new StringBuilder();
-
-      for (CharSequence line : text) {
-        if (line.length() == 0) continue;
-        if (sb.length() > 0) sb.append('\n');
-        sb.append(line);
-      }
-
-      if (sb.length() > 0) {
-        Endpoints.setPopupEndpoint(sb.toString());
-      }
-    }
-  }
-
   @Override
   public void onAccessibilityEvent (AccessibilityEvent event) {
     if (ApplicationSettings.LOG_UPDATES) {
       Log.d(LOG_TAG, "accessibility event starting: " + event.toString());
+    //say(event);
     }
 
     try {
@@ -134,6 +231,10 @@ public class ScreenMonitor extends AccessibilityService {
       AccessibilityNodeInfo source = event.getSource();
 
       switch (type) {
+        case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+          setCurrentNode(event);
+          break;
+
         case AccessibilityEvent.TYPE_VIEW_SELECTED:
           handleViewSelected(event, source);
           break;
@@ -153,15 +254,18 @@ public class ScreenMonitor extends AccessibilityService {
           logEventComponent(node, "node");
 
           switch (type) {
-            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
             case AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED:
+            case AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED:
+              break;
+
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+            case AccessibilityEvent.TYPE_VIEW_SELECTED:
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
               endpoint.write(node, true);
               break;
 
             default:
               endpoint.write(node, false);
-            case AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED:
               break;
           }
 
