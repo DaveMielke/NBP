30c93e589dcb6f81419417714d92848d680dcb16
Dave Mielke <dave@mielke.cc>
2015-07-09 23:50:58 -0400

    Rename braille.c to brldev.c. (dm)

--- b2g/ui/jni/Android.mk
+++ b2g/ui/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := UserInterface
-LOCAL_SRC_FILES := utils.c uinput.c keyboard.c kbdmon.c braille.c
+LOCAL_SRC_FILES := utils.c uinput.c keyboard.c kbdmon.c brldev.c
 LOCAL_CFLAGS := -Wall
 LOCAL_LDLIBS := -llog
 
