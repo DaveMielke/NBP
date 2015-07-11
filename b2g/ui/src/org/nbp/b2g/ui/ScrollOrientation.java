b8b18e42184e857f517e5cf1fffd238a2fec647b
Dave Mielke <dave@mielke.cc>
2015-07-11 18:03:46 -0400

    Add a scroll orientation cache. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/ScrollOrientation.java
+++ b2g/ui/src/org/nbp/b2g/ui/ScrollOrientation.java
@@ -1,5 +1,8 @@
 package org.nbp.b2g.ui;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import android.view.accessibility.AccessibilityNodeInfo;
 
 enum ScrollOrientation {
@@ -47,10 +50,22 @@ enum ScrollOrientation {
     return isEligible(node.getClassName());
   }
 
+  private final static Map<CharSequence, ScrollOrientation> orientationCache = new HashMap<CharSequence, ScrollOrientation>();
+
   public static ScrollOrientation getScrollOrientation (AccessibilityNodeInfo node) {
-    for (ScrollOrientation orientation : ScrollOrientation.values()) {
-      if (orientation.isEligible(node)) {
-        return orientation;
+    synchronized (orientationCache) {
+      CharSequence className = node.getClassName();
+
+      {
+        ScrollOrientation orientation = orientationCache.get(className);
+        if (orientation != null) return orientation;
+      }
+
+      for (ScrollOrientation orientation : ScrollOrientation.values()) {
+        if (orientation.isEligible(className)) {
+          orientationCache.put(className, orientation);
+          return orientation;
+        }
       }
     }
 
