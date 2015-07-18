b0b4729457e80d1922181f2c71abcaa6a8b5a15b
Dave Mielke <dave@mielke.cc>
2015-07-18 15:21:57 -0400

    Add file handling to the input processor object. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/InputProcessor.java
+++ b2g/ui/src/org/nbp/b2g/ui/InputProcessor.java
@@ -4,36 +4,42 @@ import android.util.Log;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-
 import java.io.InputStream;
+
+import java.io.File;
+import java.io.FileInputStream;
+
 import java.io.Reader;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.io.FileNotFoundException;
 
 public abstract class InputProcessor {
   private final static String LOG_TAG = InputProcessor.class.getName();
 
+  public final String INPUT_ENCODING = "UTF8";
+
   protected abstract boolean processLine (String text, int number);
 
   public boolean processInput (Reader reader) {
-    BufferedReader input;
+    BufferedReader buffer;
 
     if (reader instanceof BufferedReader) {
-      input = (BufferedReader)reader;
+      buffer = (BufferedReader)reader;
     } else {
-      input = new BufferedReader(reader);
+      buffer = new BufferedReader(reader);
     }
 
     for (int number=0; true; number+=1) {
       try {
-        String text = input.readLine();
+        String text = buffer.readLine();
         if (text == null) break;
         if (!processLine(text, number)) break;
       } catch (IOException exception) {
-        Log.w(LOG_TAG, "input error", exception);
+        Log.w(LOG_TAG, "line read error", exception);
         return false;
       }
     }
@@ -42,13 +48,24 @@ public abstract class InputProcessor {
   }
 
   public boolean processInput (InputStream stream) {
-    String encoding = "UTF8";
+    String encoding = INPUT_ENCODING;
 
     try {
       Reader reader = new InputStreamReader(stream, encoding);
       return processInput(reader);
     } catch (UnsupportedEncodingException exception) {
       Log.w(LOG_TAG, "unsupported input encoding: " + encoding);
+    }
+
+    return false;
+  }
+
+  public boolean processInput (File file) {
+    try {
+      InputStream stream = new FileInputStream(file);
+      return processInput(stream);
+    } catch (FileNotFoundException exception) {
+      Log.w(LOG_TAG, "file not found: " + file.toString());
     }
 
     return false;
