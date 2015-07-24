3d342390be3189555c7893481be0d1fb9ed87452
Dave Mielke <dave@mielke.cc>
2015-07-24 17:26:35 -0400

    Add a monitor for external storage events. (dm)

---
+++ b2g/ui/src/org/nbp/b2g/ui/HostMonitor.java
@@ -0,0 +1,26 @@
+package org.nbp.b2g.ui;
+
+import android.util.Log;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+
+public class HostMonitor extends BroadcastReceiver {
+  private final static String LOG_TAG = HostMonitor.class.getName();
+
+  @Override
+  public void onReceive (Context context, Intent intent) {
+    String action = intent.getAction();
+
+    if (action != null) {
+      Log.d(LOG_TAG, "host action: " + action);
+
+      if (Environment.isExternalStorageRemovable()) {
+        if (action.equals(Intent.ACTION_MEDIA_EJECT)) {
+        }
+      }
+    }
+  }
+}
