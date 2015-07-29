940dd7553b78a34cac9e154e8272527d527ce002
Dave Mielke <dave@mielke.cc>
2015-07-29 01:08:24 -0400

    Show the Wi-Fi state. (dm)

--- b2g/ui/doc/ref-firmware-bindings.rst
+++ b2g/ui/doc/ref-firmware-bindings.rst
@@ -4,8 +4,16 @@ VolumeDown + Dot7
 VolumeDown + Dot8
   Go to the Power Off screen.
 
-VolumeDown + Power Switch Off-On
-  Reboot into recovery mode. Neither braille nor speech works in recovery mode.
-  This binding is intended for a sighted person using an external video
-  monitor.
+switch the power on
+  Boot the system. If it's already booted then wake it up.
+
+switch the power off
+  Put the system to sleep in order to save power. The system itself stays up
+  but most of the peripherals (the braille display, the keyboard, etc) are shut
+  down.
+
+VolumeDown while switching the power on
+  Boot into recovery mode. This binding only applies to when the system is
+  fully shut down. Neither braille nor speech works in recovery mode. This
+  binding is intended for a sighted person using an external video monitor.
 
--- b2g/ui/src/org/nbp/b2g/ui/actions/DescribeStatus.java
+++ b2g/ui/src/org/nbp/b2g/ui/actions/DescribeStatus.java
@@ -9,9 +9,13 @@ import android.net.wifi.WifiManager;
 import android.net.wifi.WifiInfo;
 
 public class DescribeStatus extends Action {
+  private static void appendString (StringBuilder sb, int string) {
+    sb.append(ApplicationContext.getString(string));
+  }
+
   private static void startLine (StringBuilder sb, int label) {
     if (sb.length() > 0) sb.append('\n');
-    sb.append(ApplicationContext.getString(label));
+    appendString(sb, label);
     sb.append(": ");
   }
 
@@ -19,7 +23,7 @@ public class DescribeStatus extends Action {
     Bundle battery = HostMonitor.getBatteryStatus();
 
     if (battery != null) {
-      startLine(sb, R.string.describeStatus_label_battery);
+      startLine(sb, R.string.describeStatus_battery_label);
 
       {
         int level = battery.getInt(BatteryManager.EXTRA_LEVEL);
@@ -38,23 +42,49 @@ public class DescribeStatus extends Action {
     WifiManager wifi = (WifiManager)ApplicationContext.getSystemService(Context.WIFI_SERVICE);
 
     if (wifi != null) {
-      WifiInfo info = wifi.getConnectionInfo();
+      startLine(sb, R.string.describeStatus_wifi_label);
 
-      if (info != null) {
-        String name = info.getSSID();
+      switch (wifi.getWifiState()) {
+        default:
+        case WifiManager.WIFI_STATE_UNKNOWN:
+          appendString(sb, R.string.describeStatus_wifi_state_unknown);
+          break;
 
-        if (name != null) {
-          startLine(sb, R.string.describeStatus_label_wifi);
-          sb.append(name);
+        case WifiManager.WIFI_STATE_DISABLING:
+          appendString(sb, R.string.describeStatus_wifi_state_disabling);
+          break;
 
-          int dbm = info.getRssi();
-          sb.append(' ');
-          sb.append(wifi.calculateSignalLevel(dbm, 100));
-          sb.append('%');
+        case WifiManager.WIFI_STATE_DISABLED:
+          appendString(sb, R.string.describeStatus_wifi_state_disabled);
+          break;
 
-          sb.append(' ');
-          sb.append(info.getLinkSpeed());
-          sb.append("Mbps");
+        case WifiManager.WIFI_STATE_ENABLING:
+          appendString(sb, R.string.describeStatus_wifi_state_enabling);
+          break;
+
+        case WifiManager.WIFI_STATE_ENABLED: {
+          WifiInfo info = wifi.getConnectionInfo();
+
+          if (info != null) {
+            String name = info.getSSID();
+
+            if (name != null) {
+              sb.append(name);
+
+              int dbm = info.getRssi();
+              sb.append(' ');
+              sb.append(wifi.calculateSignalLevel(dbm, 100));
+              sb.append('%');
+
+              sb.append(' ');
+              sb.append(info.getLinkSpeed());
+              sb.append("Mbps");
+            }
+          } else {
+            appendString(sb, R.string.describeStatus_wifi_state_enabled);
+          }
+
+          break;
         }
       }
     }
