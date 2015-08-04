63d70759d00612c5fea51154005003c1026270ea
Dave Mielke <dave@mielke.cc>
2015-08-04 08:51:42 -0400

    Add a logs reader object. (dm)

---
+++ b2g/ui/src/org/nbp/b2g/ui/LogsReader.java
@@ -0,0 +1,145 @@
+package org.nbp.b2g.ui;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import android.util.Log;
+
+public class LogsReader {
+  private final static String LOG_TAG = LogsReader.class.getName();
+
+  public enum Format {
+    BRIEF,
+    TAG,
+    PROCESS,
+    THREAD,
+    TIME,
+    THREAD_TIME,
+    LONG,
+    RAW
+  }
+
+  private Format logFormat = Format.BRIEF;
+
+  public void setFormat (Format format) {
+    logFormat = format;
+  }
+
+  public enum Buffer {
+    MAIN,
+    SYSTEM,
+    RADIO,
+    EVENTS
+  }
+
+  private List<Buffer> logBuffers = new ArrayList<Buffer>();
+
+  public void addBuffer (Buffer buffer) {
+    logBuffers.add(buffer);
+  }
+
+  public enum Level {
+    VERBOSE,
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL,
+    SILENT
+  }
+
+  private static class Filter {
+    public final String tag;
+    private final Level level;
+
+    public Filter (String tag, Level level) {
+      this.tag = (tag != null)? tag: "*";
+      this.level = (level != null)? level: Level.INFO;
+    }
+  }
+
+  private final List<Filter> logFilters = new ArrayList<Filter>();
+
+  public void addFilter (String tag, Level level) {
+    logFilters.add(new Filter(tag, level));
+  }
+
+  public void addFilter (String tag) {
+    addFilter(tag, null);
+  }
+
+  public void addFilter (Level level) {
+    addFilter(null, level);
+  }
+
+  private List<String> makeCommand () {
+    List<String> command = new ArrayList<String>();
+
+    command.add("logcat");
+    command.add("-d");
+
+    command.add("-v");
+    command.add(logFormat.name().toLowerCase().replace("_", ""));
+
+    {
+      List<Buffer> buffers = logBuffers;
+
+      if (buffers.isEmpty()) {
+        buffers = new ArrayList<Buffer>();
+        buffers.add(Buffer.MAIN);
+        buffers.add(Buffer.SYSTEM);
+      }
+
+      for (Buffer buffer : buffers) {
+        command.add("-b");
+        command.add(buffer.name().toLowerCase());
+      }
+    }
+
+    {
+      List<Filter> filters = logFilters;
+
+      if (filters.isEmpty()) {
+        filters.add(new Filter(null, null));
+      }
+
+      for (Filter filter : filters) {
+        command.add((filter.tag + ":" + filter.level.name().charAt(0)));
+      }
+    }
+
+    return command;
+  }
+
+  public Collection<String> read () {
+    ProcessBuilder pb = new ProcessBuilder(makeCommand());
+
+    try {
+      Process process = pb.start();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+      List<String> logs = new ArrayList<String>();
+      String line;
+
+      while ((line = reader.readLine()) != null) {
+        logs.add(line);
+      }
+
+      return logs;
+    } catch (IOException exception) {
+      Log.w(LOG_TAG, "logs read error", exception);
+    }
+
+    return null;
+  }
+
+  public LogsReader () {
+  }
+}
