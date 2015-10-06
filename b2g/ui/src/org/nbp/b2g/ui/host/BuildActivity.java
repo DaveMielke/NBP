924d43f7190f014e1177c40b805ad4262f6fbcaa
Dave Mielke <dave@mielke.cc>
2015-10-06 15:49:09 -0400

    Show the build details in two columns (label, value). (dm)

--- b2g/ui/src/org/nbp/b2g/ui/host/BuildActivity.java
+++ b2g/ui/src/org/nbp/b2g/ui/host/BuildActivity.java
@@ -5,35 +5,30 @@ import android.util.Log;
 import android.os.Build;
 
 import android.view.View;
-import android.widget.TextView;
+import android.widget.GridLayout;
 
 public class BuildActivity extends ProgrammaticActivity {
   private final static String LOG_TAG = BuildActivity.class.getName();
 
-  private TextView textView;
+  private GridLayout buildDetails;
+  private int rowIndex = 0;
 
-  @Override
-  protected final View createContentView () {
-    textView = newTextView();
+  private void setColumn (int row, int column, CharSequence text) {
+    GridLayout grid = buildDetails;
 
-    return createVerticalGroup(
-      textView
+    grid.addView(
+      newTextView(text),
+      new GridLayout.LayoutParams(grid.spec(row), grid.spec(column))
     );
   }
 
-  private static void appendString (StringBuilder sb, int string) {
-    sb.append(ApplicationContext.getString(string));
+  private void addBuildDetail (int label, CharSequence value) {
+    setColumn(rowIndex, 0, getString(label));
+    setColumn(rowIndex, 1, value);
+    rowIndex += 1;
   }
 
-  private static void startLine (StringBuilder sb, int label) {
-    if (sb.length() > 0) sb.append('\n');
-    appendString(sb, label);
-    sb.append(": ");
-  }
-
-  private static boolean addBuildProperty (
-    final StringBuilder sb, String property, final int... labels
-  ) {
+  private boolean addBuildProperty (String property, final int... labels) {
     InputProcessor inputProcessor = new InputProcessor() {
       @Override
       protected final boolean handleLine (String text, int number) {
@@ -41,8 +36,7 @@ public class BuildActivity extends ProgrammaticActivity {
         text = text.trim();
 
         if (!text.isEmpty()) {
-          startLine(sb, label);
-          sb.append(text);
+          addBuildDetail(label, text);
         } else {
           Log.w(LOG_TAG, "build property not available: " + label);
         }
@@ -54,28 +48,26 @@ public class BuildActivity extends ProgrammaticActivity {
     return inputProcessor.processInput(("build." + property));
   }
 
-  private static void addBuildSystem (StringBuilder sb) {
-    startLine(sb, R.string.build_activity_label_android_build);
-    sb.append(Build.ID);
-  }
-
-  private static void addBuildFirmware (StringBuilder sb) {
-    startLine(sb, R.string.build_activity_label_firmware_version);
-    sb.append(FirmwareVersion.getMajor());
-    sb.append('.');
-    sb.append(FirmwareVersion.getMinor());
-  }
-
   @Override
-  public void onResume () {
-    super.onResume();
-    StringBuilder sb = new StringBuilder();
+  protected final View createContentView () {
+    buildDetails = new GridLayout(this);
+    buildDetails.setOrientation(buildDetails.VERTICAL);
 
-    addBuildProperty(sb, "time", R.string.build_activity_label_ui_time);
-    addBuildProperty(sb, "revision", R.string.build_activity_label_ui_revision);
-    addBuildSystem(sb);
-    addBuildFirmware(sb);
+    addBuildProperty("time", R.string.build_activity_label_ui_time);
+    addBuildProperty("revision", R.string.build_activity_label_ui_revision);
 
-    textView.setText(sb.toString());
+    addBuildDetail(R.string.build_activity_label_android_build, Build.ID);
+
+    addBuildDetail(
+      R.string.build_activity_label_firmware_version, 
+      String.format("%d.%d",
+        FirmwareVersion.getMajor(),
+        FirmwareVersion.getMinor()
+      )
+    );
+
+    return createVerticalGroup(
+      buildDetails
+    );
   }
 }
