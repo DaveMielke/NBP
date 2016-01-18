07330ece430ba25af1cbea32052813e9f43d2cc3
Dave Mielke <dave@mielke.cc>
2016-01-17 21:50:38 -0500

    Get rid of the hard-coded translation table length multiplier. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/TranslationUtilities.java
+++ b2g/ui/src/org/nbp/b2g/ui/TranslationUtilities.java
@@ -3,18 +3,48 @@ package org.nbp.b2g.ui;
 import android.util.Log;
 
 import org.liblouis.Louis;
+import org.liblouis.TranslationTable;
 import org.liblouis.BrailleTranslation;
+import org.liblouis.TextTranslation;
 
 public abstract class TranslationUtilities {
   private final static String LOG_TAG = TranslationUtilities.class.getName();
 
-  public static BrailleTranslation newBrailleTranslation (CharSequence text) {
-    if (!ApplicationSettings.LITERARY_BRAILLE) return null;
+  private static int lengthMultiplier = 1;
 
-    return Louis.getBrailleTranslation(
-      ApplicationSettings.BRAILLE_CODE.getTranslationTable(),
-      text, (text.length() * 5), -1
-    );
+  private static TranslationTable getTranslationTable () {
+    if (!ApplicationSettings.LITERARY_BRAILLE) return null;
+    return ApplicationSettings.BRAILLE_CODE.getTranslationTable();
+  }
+
+  public static BrailleTranslation newBrailleTranslation (CharSequence text) {
+    TranslationTable table = getTranslationTable();
+    if (table == null) return null;
+    final int textLength = text.length();
+
+    while (true) {
+      BrailleTranslation brl = Louis.getBrailleTranslation(
+        table, text, (textLength * lengthMultiplier), -1
+      );
+
+      if (brl.getTextLength() == textLength) return brl;
+      lengthMultiplier += 1;
+    }
+  }
+
+  public static TextTranslation newTextTranslation (CharSequence braille) {
+    TranslationTable table = getTranslationTable();
+    if (table == null) return null;
+    final int brailleLength = braille.length();
+
+    while (true) {
+      TextTranslation txt = Louis.getTextTranslation(
+        table, braille, (brailleLength * lengthMultiplier), -1
+      );
+
+      if (txt.getBrailleLength() == brailleLength) return txt;
+      lengthMultiplier += 1;
+    }
   }
 
   private TranslationUtilities () {
