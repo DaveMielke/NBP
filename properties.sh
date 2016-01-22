85e0572674029410b630a125ce2207757d6d6288
Dave Mielke <dave@mielke.cc>
2016-01-22 05:56:07 -0500

    Format the common build property values centrally. (dm)

--- properties.sh
+++ properties.sh
@@ -9,3 +9,11 @@ revisionCommit="${4#g}"
 set -- ${revisionVersion//./ }
 versionMajor="${1}"
 versionMinor="${2}"
+
+set -- $(date --utc +'%Y-%m-%d %H:%M %Z')
+buildDate="${1}"
+buildTime="${2}"
+buildZone="${3}"
+
+packageVersion="${versionMajor}.${versionMinor}.${revisionIncrement}"
+sourceRevision="git:${revisionCommit}"
