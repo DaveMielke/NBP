2c736a899645cdb89bc98c548335ca6437ebd45f
Dave Mielke <dave@mielke.cc>
2016-01-31 19:58:49 -0500

    DescribeActions should always use the host endpoint's actions. (dm)

---
+++ b2g/ui/src/org/nbp/b2g/ui/host/actions/DescribeActions.java
@@ -0,0 +1,20 @@
+package org.nbp.b2g.ui.host.actions;
+import org.nbp.b2g.ui.host.*;
+import org.nbp.b2g.ui.*;
+
+public class DescribeActions extends Action {
+  @Override
+  public boolean performAction () {
+    ActionChooser.chooseAction(Endpoints.host
+                                        .get()
+                                        .getKeyBindings()
+                                        .getRootKeyBindingMap()
+    );
+
+    return true;
+  }
+
+  public DescribeActions (Endpoint endpoint) {
+    super(endpoint, false);
+  }
+}
