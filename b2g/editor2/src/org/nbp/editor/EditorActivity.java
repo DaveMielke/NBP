fc334819ee6f65782e1367a3d9f90ac68932185d
Dave Mielke <dave@mielke.cc>
2016-02-02 12:50:52 -0500

    Set up a basic main screen for the editor. (dm)

--- b2g/editor2/src/org/nbp/editor/EditorActivity.java
+++ b2g/editor2/src/org/nbp/editor/EditorActivity.java
@@ -3,13 +3,10 @@ package org.nbp.editor;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class EditorActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class EditorActivity extends Activity {
+  @Override
+  public void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.editor_activity);
+  }
 }
