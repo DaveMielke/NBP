393816305efe63e816c6650e8fb89d3d362694c5
Dave Mielke <dave@mielke.cc>
2016-02-02 11:03:52 -0500

    Create a project for the new (Java-based) editor. (dm)

---
+++ b2g/editor2/src/org/nbp/editor/EditorActivity.java
@@ -0,0 +1,15 @@
+package org.nbp.editor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditorActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
