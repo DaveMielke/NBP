126bdb799b702371d8cd366d4b99b4a12741cc11
Dave Mielke <dave@mielke.cc>
2016-02-02 23:26:42 -0500

    Add a control character input filter to the editor. (dm)

--- editor/src/org/nbp/editor/EditorActivity.java
+++ editor/src/org/nbp/editor/EditorActivity.java
@@ -7,8 +7,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Button;
+
+import android.widget.EditText;
+import android.text.InputFilter;
+import android.text.Spanned;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,51 +22,10 @@ import com.aspose.words.License;
 public class EditorActivity extends Activity {
   private final static String LOG_TAG = EditorActivity.class.getName();
 
-  private TextView editArea = null;
+  private EditText editArea = null;
 
   protected final Activity getActivity () {
     return this;
-  }
-
-  private final void prepareAsposeWords () {
-    new Thread() {
-      @Override
-      public void run () {
-        AsposeWordsApplication app = new AsposeWordsApplication();
-        app.loadLibs(getActivity());
-
-        try {
-          License license = new License();
-          license.setLicense(getAssets().open("Aspose.Words.lic"));
-          Log.d(LOG_TAG, "Aspose Words license set");
-        } catch (Exception exception) {
-          Log.w(LOG_TAG, ("Aspose Words license failure: " + exception.getMessage()));
-        }
-      }
-    }.start();
-  }
-
-  @Override
-  public void onCreate (Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    prepareAsposeWords();
-
-    setContentView(R.layout.editor);
-    editArea = (TextView)findViewById(R.id.edit_area);
-
-    if (getActionBar() == null) {
-      Button button = (Button)findViewById(R.id.actions_button);
-      button.setVisibility(button.VISIBLE);
-
-      button.setOnClickListener(
-        new Button.OnClickListener() {
-          @Override
-          public void onClick (View view) {
-            getActivity().openOptionsMenu();
-          }
-        }
-      );
-    }
   }
 
   @Override
@@ -113,5 +75,94 @@ public class EditorActivity extends Activity {
       default:
         return false;
     }
+  }
+
+  private final void prepareAsposeWords () {
+    new Thread() {
+      @Override
+      public void run () {
+        AsposeWordsApplication app = new AsposeWordsApplication();
+        app.loadLibs(getActivity());
+
+        try {
+          License license = new License();
+          license.setLicense(getAssets().open("Aspose.Words.lic"));
+          Log.d(LOG_TAG, "Aspose Words license set");
+        } catch (Exception exception) {
+          Log.w(LOG_TAG, ("Aspose Words license failure: " + exception.getMessage()));
+        }
+      }
+    }.start();
+  }
+
+  private final void prepareActionsButton () {
+    if (getActionBar() == null) {
+      Button button = (Button)findViewById(R.id.actions_button);
+      button.setVisibility(button.VISIBLE);
+
+      button.setOnClickListener(
+        new Button.OnClickListener() {
+          @Override
+          public void onClick (View view) {
+            getActivity().openOptionsMenu();
+          }
+        }
+      );
+    }
+  }
+
+  private void setInputFilters () {
+    editArea.setFilters(
+      new InputFilter[] {
+        new InputFilter() {
+          @Override
+          public CharSequence filter (
+            CharSequence src, int srcStart, int srcEnd,
+            Spanned dst, int dstStart, int dstEnd
+          ) {
+            while (srcStart < srcEnd) {
+              char character = src.charAt(srcStart++);
+
+              switch (character) {
+                case 0X0E:
+                  newContent();
+                  break;
+
+                case 0X0F:
+                  openContent();
+                  break;
+
+                case 0X13:
+                  saveContent();
+                  break;
+
+                default:
+                  if (character < 0X20) return "";
+
+                case '\f':
+                case '\n':
+                case '\r':
+                case '\t':
+                  break;
+              }
+            }
+
+            return null;
+          }
+        }
+      }
+    );
+  }
+
+  @Override
+  public void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    prepareAsposeWords();
+
+    setContentView(R.layout.editor);
+    editArea = (EditText)findViewById(R.id.edit_area);
+
+    prepareActionsButton();
+    setInputFilters();
   }
 }
