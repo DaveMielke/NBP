e158fe18d1bfe78c7f9a8d8d8df2185de1bbdf86
Dave Mielke <dave@mielke.cc>
2016-02-08 20:06:59 -0500

    Define an easy-to-find constant for the color cache size. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/Colors.java
+++ b2g/ui/src/org/nbp/b2g/ui/Colors.java
@@ -13,8 +13,10 @@ import android.graphics.Color;
 public abstract class Colors {
   private final static String LOG_TAG = Colors.class.getName();
 
+  private final static int CACHE_SIZE = 0X10;
+
   private final static Map<Integer, String> knownColorNames = new HashMap<Integer, String>();
-  private final static Map<Integer, String> cachedColorNames = new CachingMap<Integer, String>(0X10);
+  private final static Map<Integer, String> cachedColorNames = new CachingMap<Integer, String>(CACHE_SIZE);
 
   private static int normalizedColor (int color) {
     return Color.rgb(
