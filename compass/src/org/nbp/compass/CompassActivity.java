db844d8504dde0653c56f8ca61c25f7baac6ba03
Dave Mielke <dave@mielke.cc>
2016-02-23 17:22:22 -0500

    Implement more of the compass. (dm)

--- compass/src/org/nbp/compass/CompassActivity.java
+++ compass/src/org/nbp/compass/CompassActivity.java
@@ -3,13 +3,54 @@ package org.nbp.compass;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class CompassActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.widget.TextView;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class CompassActivity extends Activity implements SensorEventListener {
+  private SensorManager sensorManager;
+  private TextView headingView;
+
+  @Override
+  public void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+    setContentView(R.layout.compass);
+    headingView = (TextView)findViewById(R.id.heading);
+  }
+
+  @Override
+  protected void onResume () {
+    super.onResume();
+
+    sensorManager.registerListener(this,
+      sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+      SensorManager.SENSOR_DELAY_GAME
+    );
+  }
+
+  @Override
+  protected void onPause () {
+    super.onPause();
+
+    sensorManager.unregisterListener(this);
+  }
+
+  @Override
+  public void onSensorChanged (SensorEvent event) {
+    int degrees = Math.round(event.values[0]);
+
+    headingView.setText(String.format(
+      "%d",
+      degrees
+    ));
+  }
+
+  @Override
+  public void onAccuracyChanged (Sensor sensor, int accuracy) {
+  }
 }
