0482303cfdcfbc905fadaa6dd99ca44282c4aef7
Dave Mielke <dave@mielke.cc>
2016-02-29 16:34:17 -0500

    Add pitch, roll, and accuracy to the compass output. (dm)

--- compass/src/org/nbp/compass/CompassActivity.java
+++ compass/src/org/nbp/compass/CompassActivity.java
@@ -1,5 +1,8 @@
 package org.nbp.compass;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import android.util.Log;
 
 import android.app.Activity;
@@ -15,6 +18,12 @@ import android.hardware.SensorManager;
 public class CompassActivity extends Activity implements SensorEventListener {
   private final static String LOG_TAG = CompassActivity.class.getName();
 
+  private final Map<Integer, String> accuracyNames = new HashMap<Integer, String>();
+
+  private final void addAccuracy (int value, int name) {
+    accuracyNames.put(value, getString(name));
+  }
+
   private SensorManager sensorManager;
   private TextView headingView;
 
@@ -25,6 +34,11 @@ public class CompassActivity extends Activity implements SensorEventListener {
 
     setContentView(R.layout.compass);
     headingView = (TextView)findViewById(R.id.heading);
+
+    addAccuracy(SensorManager.SENSOR_STATUS_UNRELIABLE, R.string.accuracy_unreliable);
+    addAccuracy(SensorManager.SENSOR_STATUS_ACCURACY_LOW, R.string.accuracy_low);
+    addAccuracy(SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM, R.string.accuracy_medium);
+    addAccuracy(SensorManager.SENSOR_STATUS_ACCURACY_HIGH, R.string.accuracy_high);
   }
 
   @Override
@@ -56,17 +70,25 @@ public class CompassActivity extends Activity implements SensorEventListener {
 
   @Override
   public void onSensorChanged (SensorEvent event) {
-    float degrees = event.values[0];
+    float heading = event.values[0];
+    float pitch   = event.values[1];
+    float roll    = event.values[2];
 
-    if ((degrees >= 0f) && (degrees < DEGREES_PER_CIRCLE)) {
-      int direction = Math.round(degrees / DEGREES_PER_DIRECTION);
+    String accuracy = accuracyNames.get(event.accuracy);
+    if (accuracy == null) accuracy = getString(R.string.accuracy_unknown);
+
+    if ((heading >= 0f) && (heading < DEGREES_PER_CIRCLE)) {
+      int direction = Math.round(heading / DEGREES_PER_DIRECTION);
       direction %= DIRECTION_COUNT;
 
       headingView.setText(String.format(
-        "%s%+d [%d]",
+        "%s: %d° [%s%+d]\n%s: %d°\n%s: %d°\n%s: %s",
+        getString(R.string.label_heading), Math.round(heading),
         directions[direction],
-        Math.round(degrees - ((float)direction * DEGREES_PER_DIRECTION)),
-        Math.round(degrees)
+        Math.round(heading - ((float)direction * DEGREES_PER_DIRECTION)),
+        getString(R.string.label_pitch), Math.round(pitch),
+        getString(R.string.label_roll), Math.round(roll),
+        getString(R.string.label_accuracy), accuracy
       ));
     }
   }
