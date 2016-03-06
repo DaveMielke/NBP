dd1328b04b5803cb38ceb3dce083ba67ba845447
Dave Mielke <dave@mielke.cc>
2016-03-06 14:47:38 -0500

    Add latitude and longitude to the compass screen. (dm)

--- compass/src/org/nbp/compass/CompassActivity.java
+++ compass/src/org/nbp/compass/CompassActivity.java
@@ -7,31 +7,45 @@ import android.os.Bundle;
 
 import android.widget.TextView;
 
+import android.hardware.SensorManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 
-public class CompassActivity extends Activity implements SensorEventListener {
+import android.location.LocationManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.Criteria;
+
+public class CompassActivity
+  extends Activity
+  implements SensorEventListener, LocationListener
+{
   private final static String LOG_TAG = CompassActivity.class.getName();
 
   private TextView azimuthDegrees;
   private TextView azimuthDirection;
   private TextView pitchDegrees;
   private TextView rollDegrees;
+  private TextView latitudeDecimal;
+  private TextView latitudeDMS;
+  private TextView longitudeDecimal;
+  private TextView longitudeDMS;
+
+  private SensorManager sensorManager;
+  private LocationManager locationManager;
 
   private final static int[] sensorTypes = new int[] {
     Sensor.TYPE_ACCELEROMETER,
     Sensor.TYPE_MAGNETIC_FIELD
   };
 
-  private SensorManager sensorManager;
   private final Sensor[] sensorArray = new Sensor[sensorTypes.length];
-  private final float[] rotationMatrix = new float[9];
-  private final float[] currentOrientation = new float[3];
-
   private float[] gravityVector = null;
   private float[] geomagneticVector = null;
+
+  private final float[] rotationMatrix = new float[9];
+  private final float[] currentOrientation = new float[3];
 
   private final Measurement azimuthMeasurement = new Measurement();
   private final Measurement pitchMeasurement = new Measurement();
@@ -46,8 +60,13 @@ public class CompassActivity extends Activity implements SensorEventListener {
     azimuthDirection = (TextView)findViewById(R.id.azimuth_direction);
     pitchDegrees = (TextView)findViewById(R.id.pitch_degrees);
     rollDegrees = (TextView)findViewById(R.id.roll_degrees);
+    latitudeDecimal = (TextView)findViewById(R.id.latitude_decimal);
+    latitudeDMS = (TextView)findViewById(R.id.latitude_dms);
+    longitudeDecimal = (TextView)findViewById(R.id.longitude_decimal);
+    longitudeDMS = (TextView)findViewById(R.id.longitude_dms);
 
     sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+    locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 
     {
       int count = 0;
@@ -56,6 +75,8 @@ public class CompassActivity extends Activity implements SensorEventListener {
         sensorArray[count++] = sensorManager.getDefaultSensor(type);
       }
     }
+
+    setLocation(locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER));
   }
 
   @Override
@@ -65,6 +86,10 @@ public class CompassActivity extends Activity implements SensorEventListener {
     for (Sensor sensor : sensorArray) {
       sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
     }
+
+    locationManager.requestLocationUpdates(
+      LocationManager.GPS_PROVIDER, 5000, 10f, this
+    );
   }
 
   @Override
@@ -74,6 +99,8 @@ public class CompassActivity extends Activity implements SensorEventListener {
     for (Sensor sensor : sensorArray) {
       sensorManager.unregisterListener(this, sensor);
     }
+
+    locationManager.removeUpdates(this);
   }
 
   private final float translateValue (Measurement measurement, float value) {
@@ -153,5 +180,76 @@ public class CompassActivity extends Activity implements SensorEventListener {
 
   @Override
   public void onAccuracyChanged (Sensor sensor, int accuracy) {
+  }
+
+  private final void setLocation (
+    double degrees,
+    TextView decimal, TextView dms,
+    char positive, char negative
+  ) {
+    decimal.setText(String.format("%.4f°", degrees));
+
+    char hemisphere;
+
+    if (degrees < 0f) {
+      hemisphere = negative;
+      degrees = -degrees;
+    } else {
+      hemisphere = positive;
+    }
+
+    long value = Math.round(degrees * 3600f);
+    if (value == 0) hemisphere = ' ';
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(value % 60);
+    sb.append('"');
+    value /= 60;
+
+    if (value > 0) {
+      sb.insert(0, "' ");
+      sb.insert(0, value%60);
+      value /= 60;
+
+      if (value > 0) {
+        sb.insert(0, "° ");
+        sb.insert(0, value);
+      }
+    }
+
+    if (hemisphere != ' ') {
+      sb.append(' ');
+      sb.append(hemisphere);
+    }
+
+    dms.setText(sb.toString());
+  }
+
+  private final void setLocation (double latitude, double longitude) {
+    setLocation(latitude, latitudeDecimal, latitudeDMS, 'N', 'S');
+    setLocation(longitude, longitudeDecimal, longitudeDMS, 'E', 'W');
+  }
+
+  private final void setLocation (Location location) {
+    if (location != null) {
+      setLocation(location.getLatitude(), location.getLongitude());
+    }
+  }
+
+  @Override
+  public void onLocationChanged (Location location) {
+    setLocation(location);
+  }
+
+  @Override
+  public void onProviderDisabled (String provider) {
+  }
+
+  @Override
+  public void onProviderEnabled (String provider) {
+  }
+
+  @Override
+  public void onStatusChanged (String provider, int status, Bundle extras) {
   }
 }
