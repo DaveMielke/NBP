b70b9b34f5b00d6400d5a9de0914194f64bea182
Dave Mielke <dave@mielke.cc>
2016-03-08 10:34:36 -0500

    Add the skeleton for the calculator. (dm)

---
+++ calculator/src/org/nbp/calculator/CalculatorActivity.java
@@ -0,0 +1,12 @@
+package org.nbp.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends Activity {
+  @Override
+  protected void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.calculator);
+  }
+}
