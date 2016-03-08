0e68197f45c7a8f4bf7413d1f23940dc90fcfa9c
Dave Mielke <dave@mielke.cc>
2016-03-08 12:36:57 -0500

    All NBP apps should use a common set of custom xml rules. (dm)

--- calculator/src/org/nbp/calculator/CalculatorActivity.java
+++ calculator/src/org/nbp/calculator/CalculatorActivity.java
@@ -3,10 +3,16 @@ package org.nbp.calculator;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.widget.EditText;
+
 public class CalculatorActivity extends Activity {
+  private EditText expressionView;
+
   @Override
   protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.calculator);
+    expressionView = (EditText)findViewById(R.id.expression);
   }
 }
