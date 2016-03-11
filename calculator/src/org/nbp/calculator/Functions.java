fa2a0dd8fa9f5ed7347eba537538dfa11500f5fd
Dave Mielke <dave@mielke.cc>
2016-03-10 23:55:25 -0500

    Add more functions. (dm)

--- calculator/src/org/nbp/calculator/Functions.java
+++ calculator/src/org/nbp/calculator/Functions.java
@@ -12,7 +12,61 @@ public abstract class Functions {
   private final static FunctionMap systemFunctions = new FunctionMap();
 
   static {
-    systemFunctions.put("deg",
+    systemFunctions.put("abs",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.abs(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("round",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.rint(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("floor",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.floor(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("ceil",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.ceil(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("sqrt",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.sqrt(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("cbrt",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.cbrt(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("r2d",
       new Function() {
         @Override
         public double call (double argument) {
@@ -21,7 +75,7 @@ public abstract class Functions {
       }
     );
 
-    systemFunctions.put("rad",
+    systemFunctions.put("d2r",
       new Function() {
         @Override
         public double call (double argument) {
@@ -53,6 +107,60 @@ public abstract class Functions {
         @Override
         public double call (double argument) {
           return Math.tan(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("asin",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.asin(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("acos",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.acos(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("atan",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.atan(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("sinh",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.sinh(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("cosh",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.cosh(argument);
+        }
+      }
+    );
+
+    systemFunctions.put("tanh",
+      new Function() {
+        @Override
+        public double call (double argument) {
+          return Math.tanh(argument);
         }
       }
     );
