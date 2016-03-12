90419991b7efe835ace15ac6e30488f4a3377457
Dave Mielke <dave@mielke.cc>
2016-03-12 17:12:00 -0500

    Improve the formatting of the result. (dm)

--- calculator/src/org/nbp/calculator/CalculatorActivity.java
+++ calculator/src/org/nbp/calculator/CalculatorActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class CalculatorActivity extends CommonActivity {
   private EditText expressionView;
   private TextView resultView;
@@ -33,14 +36,68 @@ public class CalculatorActivity extends CommonActivity {
     );
   }
 
+  private final static Pattern REAL_PATTERN = Pattern.compile(
+    "^([-+])?0*(\\d*?)(?:\\.(\\d*?)0*)?(?:[eE]([-+])?0*(\\d+?))?$"
+  );
+
+  private final String getMatch (String string, Matcher matcher, int group) {
+    int start = matcher.start(group);
+    if (start < 0) return "";
+
+    int end = matcher.end(group);
+    if (end < 0) return "";
+
+    return string.substring(start, end);
+  }
+
   private final void evaluateExpression () {
     String expression = expressionView.getText().toString();
 
     try {
       ExpressionEvaluation evaluation = new ExpressionEvaluation(expression);
       double result = evaluation.getResult();
+      String text = String.format("%.12E", evaluation.getResult());
+      Matcher matcher = REAL_PATTERN.matcher(text);
 
-      resultView.setText(Double.toString(result));
+      if (matcher.lookingAt()) {
+        String sign = getMatch(text, matcher, 1);
+        String before = getMatch(text, matcher, 2);
+        String after = getMatch(text, matcher, 3);
+        String exponentSign = getMatch(text, matcher, 4);
+        String exponentValue = getMatch(text, matcher, 5);
+
+        if (!exponentSign.equals("-")) exponentSign = "";
+        int exponent = Integer.valueOf((exponentSign + exponentValue));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(before);
+        exponent += before.length();
+        sb.append(after);
+
+        if ((exponent >= 1) && (exponent <= 12)) {
+          if (sb.length() > exponent) {
+            sb.insert(exponent, '.');
+          } else {
+            while (sb.length() < exponent) sb.append('0');
+          }
+
+          while ((exponent -= 3) > 0) sb.insert(exponent, ',');
+          exponent = 0;
+        } else {
+          if (sb.length() > 1) sb.insert(1, '.');
+          exponent -= 1;
+        }
+
+        if (exponent != 0) {
+          sb.append("×10^");
+          sb.append(exponent);
+        }
+
+        if (sign.equals("-")) sb.insert(0, sign);
+        text = sb.toString();
+      }
+
+      resultView.setText(text);
       Variables.set(Variables.RESULT, result);
     } catch (ExpressionException exception) {
       resultView.setText(exception.getMessage());
