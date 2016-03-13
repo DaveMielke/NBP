d3dc5b1e66685308247d2f863a3389d6c7860ebc
Dave Mielke <dave@mielke.cc>
2016-03-12 21:44:35 -0500

    Add the beginnings of the NBP Phone app. (dm)

---
+++ phone/src/org/nbp/phone/PhoneActivity.java
@@ -0,0 +1,13 @@
+package org.nbp.phone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhoneActivity extends Activity {
+  @Override
+  protected void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.phone);
+  }
+}
