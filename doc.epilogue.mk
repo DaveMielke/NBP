e6f79e15f1a92f0f819d65ec372a200f50759bec
Dave Mielke <dave@mielke.cc>
2016-03-29 16:14:10 -0400

    Move the document configuration files to a general location. (dm)

--- doc.epilogue.mk
+++ doc.epilogue.mk
@@ -2,13 +2,13 @@ html: $(DOCUMENT_NAMES:=.html)
 txt: $(DOCUMENT_NAMES:=.txt)
 
 %.html: %/main.rst $(LOCAL_FILES)
-	rst2html --config docutils.conf $< $@
+	rst2html --config $(TOP)docutils.conf $< $@
 
 %.txt: %.html
 	lynx -dump -stderr -width=10000 -dont_wrap_pre -nolist -nonumbers $< >$@
 	sed -e '/^ *Contents *$$/,/^ *$$/d' -i $@
 #	links -dump -dump-charset utf-8 -no-references -no-numbering $< >$@
-#	html2text -rcfile html2text.conf -nobs -o $@ $<
+#	html2text -rcfile $(TOP)html2text.conf -nobs -o $@ $<
 
 clean:
 	-rm -f -- *.html
