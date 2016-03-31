aad5f3ba3cd092d6648545fbb4415ef672298b92
Dave Mielke <dave@mielke.cc>
2016-03-31 16:52:57 -0400

    Remove the original editor and launcher (shell). (dm)

--- Make/prologue.sh
+++ Make/prologue.sh
@@ -7,14 +7,31 @@ relativePath() {
 }
 
 programDirectory="$(relativePath "${0}")"
+programName="${0##*/}"
+
+programMessage() {
+  local message="${1}"
+
+  echo >&2 "${programName}: ${message}"
+}
+
+semanticError() {
+  local message="${1}"
+
+  programMessage "${message}"
+  exit 3
+}
 
 runApplicationScript() {
+  local script="application.sh"
   local directory="."
 
   while true
   do
-    local script="${directory}/application.sh"
-    [ ! -f "${script}" ] || break
+    local path="${directory}/${script}"
+    [ ! -f "${path}" ] || break
+
+    [ ! -d "${directory}/.git" ] || semanticError "application script not found"
     directory="${directory}/.."
   done
 
--- b2g/shell/project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-resolvers += Resolver.url("scalasbt snapshots", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-snapshots"))(Resolver.ivyStylePatterns)
-
-addSbtPlugin("org.scala-sbt" % "sbt-android-plugin" % "0.6.3-SNAPSHOT")
