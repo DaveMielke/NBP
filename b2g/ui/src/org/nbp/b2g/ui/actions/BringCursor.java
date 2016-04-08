26cb6ccd5e772fbe82a67a5640fb86ba9100792e
Dave Mielke <dave@mielke.cc>
2016-04-08 12:12:33 -0400

    Any cursor routing key now clicks the item when it's not an input area. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/actions/BringCursor.java
+++ b2g/ui/src/org/nbp/b2g/ui/actions/BringCursor.java
@@ -4,9 +4,20 @@ import org.nbp.b2g.ui.*;
 public class BringCursor extends CursorKeyAction {
   @Override
   protected final boolean performCursorKeyAction (Endpoint endpoint, int offset) {
-    if (!endpoint.isInputArea()) return false;
-    offset += endpoint.getLineStart();
-    return endpoint.setCursor(offset);
+    if (endpoint.isInputArea()) {
+      offset += endpoint.getLineStart();
+      return endpoint.setCursor(offset);
+    }
+
+    {
+      Action action = endpoint.getKeyBindings().getAction(KeyMask.DPAD_CENTER);
+
+      if (action != null) {
+        return KeyEvents.performAction(action);
+      }
+    }
+
+    return false;
   }
 
   public BringCursor (Endpoint endpoint) {
