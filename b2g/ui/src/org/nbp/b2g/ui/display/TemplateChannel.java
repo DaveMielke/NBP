9461525cff855a0fa8eb92d89ce165fec00fdb9b
Dave Mielke <dave@mielke.cc>
2016-05-18 13:18:16 -0400

    Add template objects for protocol and channel. (dm)

---
+++ b2g/ui/src/org/nbp/b2g/ui/display/TemplateChannel.java
@@ -0,0 +1,30 @@
+package org.nbp.b2g.ui.display;
+import org.nbp.b2g.ui.*;
+
+public class TemplateChannel extends Channel {
+  public TemplateChannel () {
+    super();
+  }
+
+  @Override
+  protected final void runChannelThread () {
+  }
+
+  @Override
+  protected final void initializeChannelThread () {
+  }
+
+  @Override
+  protected final void stopChannelThread () {
+  }
+
+  @Override
+  public final boolean send (byte b) {
+    return false;
+  }
+
+  @Override
+  public final boolean flush () {
+    return false;
+  }
+}
---
+++ b2g/ui/src/org/nbp/b2g/ui/display/TemplateProtocol.java
@@ -0,0 +1,36 @@
+package org.nbp.b2g.ui.display;
+import org.nbp.b2g.ui.*;
+
+public class TemplateProtocol extends Protocol {
+  @Override
+  public final void resetInput () {
+  }
+
+  @Override
+  public final boolean handleTimeout () {
+    return true;
+  }
+
+  @Override
+  public final boolean handleInput (byte b) {
+    return true;
+  }
+
+  @Override
+  public final int handleNavigationKeyEvent (int keyMask, boolean press) {
+    return keyMask;
+  }
+
+  @Override
+  public final boolean handleCursorKeyEvent (int keyNumber, boolean press) {
+    return false;
+  }
+
+  @Override
+  public final void resetKeys () {
+  }
+
+  public TemplateProtocol () {
+    super();
+  }
+}
