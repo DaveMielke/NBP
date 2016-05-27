8f526645e69874d72e0e627a55072c998fc1f904
Dave Mielke <dave@mielke.cc>
2016-05-27 00:43:36 -0400

    DescribeHighlighting should show the foreground and background colors as well as if the character is selected. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/actions/DescribeHighlighting.java
+++ b2g/ui/src/org/nbp/b2g/ui/actions/DescribeHighlighting.java
@@ -3,58 +3,103 @@ import org.nbp.b2g.ui.*;
 
 import org.nbp.common.HighlightSpans;
 import android.text.style.CharacterStyle;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.BackgroundColorSpan;
+
 import android.text.Spanned;
 
 public class DescribeHighlighting extends CursorKeyAction {
+  private final static void appendString (StringBuilder sb, int string) {
+    sb.append(ApplicationContext.getString(string));
+  }
+
+  private final static void startLine (StringBuilder sb, int label) {
+    if (sb.length() > 0) sb.append('\n');
+    appendString(sb, label);
+  }
+
+  private final static void describeColor (StringBuilder sb, Integer color, int label) {
+    if (color != null) {
+      startLine(sb, label);
+      sb.append(' ');
+      sb.append(Colors.getName(color));
+    }
+  }
+
   @Override
   protected final boolean performCursorKeyAction (Endpoint endpoint, int offset) {
     CharSequence text = getEndpoint().getLineText();
-    boolean bold = false;
-    boolean italic = false;
-    boolean strike = false;
-    boolean underline = false;
+    StringBuilder sb = new StringBuilder();
 
-    if (text instanceof Spanned) {
-      Spanned spanned = (Spanned)text;
-      CharacterStyle[] spans = spanned.getSpans(offset, offset+1, CharacterStyle.class);
+    {
+      int start = endpoint.getSelectionStart();
+      int end = endpoint.getSelectionEnd();
 
-      if (spans != null) {
-        for (CharacterStyle span : spans) {
-          if (spanned.getSpanStart(span) == spanned.getSpanEnd(span)) continue;
+      int adjustment = endpoint.getLineStart();
+      start -= adjustment;
+      end -= adjustment;
 
-          if (HighlightSpans.BOLD.isFor(span)) {
-            bold = true;
-          } else if (HighlightSpans.BOLD_ITALIC.isFor(span)) {
-            bold = true;
-            italic = true;
-          } else if (HighlightSpans.ITALIC.isFor(span)) {
-            italic = true;
-          } else if (HighlightSpans.STRIKE.isFor(span)) {
-            strike = true;
-          } else if (HighlightSpans.UNDERLINE.isFor(span)) {
-            underline = true;
-          }
-        }
+      if ((start <= offset) && (offset < end)) {
+        appendString(sb, R.string.DescribeHighlighting_selected);
       }
     }
 
-    int[] styles = new int[4];
-    int count = 0;
+    Integer foregroundColor = null;
+    Integer backgroundColor = null;
 
-    if (bold) styles[count++] = R.string.DescribeHighlighting_bold;
-    if (italic) styles[count++] = R.string.DescribeHighlighting_italic;
-    if (strike) styles[count++] = R.string.DescribeHighlighting_strike;
-    if (underline) styles[count++] = R.string.DescribeHighlighting_underline;
+    {
+      boolean bold = false;
+      boolean italic = false;
+      boolean strike = false;
+      boolean underline = false;
 
-    if (count == 0) styles[count++] = R.string.DescribeHighlighting_none;
-    StringBuilder sb = new StringBuilder();
+      if (text instanceof Spanned) {
+        Spanned spanned = (Spanned)text;
+        CharacterStyle[] spans = spanned.getSpans(offset, offset+1, CharacterStyle.class);
 
-    for (int index=0; index<count; index+=1) {
-      if (sb.length() > 0) sb.append(' ');
-      sb.append(ApplicationContext.getString(styles[index]));
+        if (spans != null) {
+          for (CharacterStyle span : spans) {
+            if (spanned.getSpanStart(span) == spanned.getSpanEnd(span)) continue;
+
+            if (HighlightSpans.BOLD.isFor(span)) {
+              bold = true;
+            } else if (HighlightSpans.BOLD_ITALIC.isFor(span)) {
+              bold = true;
+              italic = true;
+            } else if (HighlightSpans.ITALIC.isFor(span)) {
+              italic = true;
+            } else if (HighlightSpans.STRIKE.isFor(span)) {
+              strike = true;
+            } else if (HighlightSpans.UNDERLINE.isFor(span)) {
+              underline = true;
+            } else if (span instanceof ForegroundColorSpan) {
+              foregroundColor = ((ForegroundColorSpan)span).getForegroundColor();
+            } else if (span instanceof BackgroundColorSpan) {
+              backgroundColor = ((BackgroundColorSpan)span).getBackgroundColor();
+            }
+          }
+        }
+      }
+
+      int[] styleStrings = new int[4];
+      int count = 0;
+
+      if (bold) styleStrings[count++] = R.string.DescribeHighlighting_bold;
+      if (italic) styleStrings[count++] = R.string.DescribeHighlighting_italic;
+      if (strike) styleStrings[count++] = R.string.DescribeHighlighting_strike;
+      if (underline) styleStrings[count++] = R.string.DescribeHighlighting_underline;
+      if (count == 0) styleStrings[count++] = R.string.DescribeHighlighting_none;
+
+      for (int index=0; index<count; index+=1) {
+        if (sb.length() > 0) sb.append(' ');
+        appendString(sb, styleStrings[index]);
+      }
     }
 
-    ApplicationUtilities.message(sb.toString());
+    describeColor(sb, foregroundColor, R.string.DescribeHighlighting_foreground);
+    describeColor(sb, backgroundColor, R.string.DescribeHighlighting_background);
+
+    Endpoints.setPopupEndpoint(sb.toString());
     return true;
   }
 
