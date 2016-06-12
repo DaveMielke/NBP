8a37eb7595f470798ed7a47974d0fdaef055ea69
Dave Mielke <dave@mielke.cc>
2016-06-12 14:58:54 -0400

    Reading with Aspose should extend existing highlight spans if possible. (dm)

--- editor/src/org/nbp/editor/AsposeWordsOperations.java
+++ editor/src/org/nbp/editor/AsposeWordsOperations.java
@@ -73,6 +73,20 @@ public class AsposeWordsOperations implements ContentOperations {
   }
 
   private final void addSpan (SpannableStringBuilder content, int start, HighlightSpans.Entry spanEntry) {
+    if (start > 0) {
+      CharacterStyle[] spans = content.getSpans(start-1, start, CharacterStyle.class);
+
+      if (spans != null) {
+        for (CharacterStyle span : spans) {
+          if (spanEntry.isFor(span)) {
+            content.setSpan(span, content.getSpanStart(span),
+                            content.length(), content.getSpanFlags(span));
+            return;
+          }
+        }
+      }
+    }
+
     addSpan(content, start, spanEntry.newInstance());
   }
 
