cf90d725b11aa4000a9742d5713404dc8f77de5f
Dave Mielke <dave@mielke.cc>
2016-08-10 20:10:23 -0400

    Start to add key listener support to the alert dialog builder. (dm)

--- common/src/org/nbp/common/AlertDialogBuilder.java
+++ common/src/org/nbp/common/AlertDialogBuilder.java
@@ -1,12 +1,68 @@
 package org.nbp.common;
 
+import java.util.Map;
+import java.util.HashMap;
+
+import android.util.Log;
+
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.ListView;
+
 import android.content.Context;
+import android.view.KeyEvent;
 import android.view.ContextThemeWrapper;
 
 public class AlertDialogBuilder extends AlertDialog.Builder {
-  public AlertDialogBuilder (Context context, int... subtitles) {
-    super(context);
+  private final static String LOG_TAG = AlertDialogBuilder.class.getName();
+
+  private interface ListViewOperation {
+    public void perform (ListView listView);
+  }
+
+  private final static Map<Integer, ListViewOperation> listViewOperations =
+               new HashMap<Integer, ListViewOperation>() {
+    {
+      put(KeyEvent.KEYCODE_DPAD_DOWN,
+        new ListViewOperation() {
+          @Override
+          public void perform (ListView listView) {
+          }
+        }
+      );
+    }
+  };
+
+  private final void setKeyListener () {
+    setOnKeyListener(
+      new DialogInterface.OnKeyListener() {
+        @Override
+        public boolean onKey (DialogInterface dialogInterface, int keyCode, KeyEvent keyEvent) {
+          Log.d(LOG_TAG, ("key code: " + keyCode));
+
+          AlertDialog alertDialog = (AlertDialog)dialogInterface;
+          ListView listView = alertDialog.getListView();
+
+          if (listView != null) {
+            ListViewOperation operation = listViewOperations.get(keyCode);
+
+            if (operation != null) {
+              if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                operation.perform(listView);
+              }
+
+              return true;
+            }
+          }
+
+          return false;
+        }
+      }
+    );
+  }
+
+  private final void setTitle (int... subtitles) {
+    Context context = getContext();
 
     StringBuilder title = new StringBuilder();
     title.append(context.getString(R.string.app_name));
@@ -17,5 +73,11 @@ public class AlertDialogBuilder extends AlertDialog.Builder {
     }
 
     setTitle(title.toString());
+  }
+
+  public AlertDialogBuilder (Context context, int... subtitles) {
+    super(context);
+    setTitle(subtitles);
+    setKeyListener();
   }
 }
