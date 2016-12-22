d04864db5a3de320885defcbdb2d6ce642125f99
Dave Mielke <dave@mielke.cc>
2016-12-22 16:32:31 -0500

    The Unicode braille dots should be chars (not bytes). (dm)

--- common/src/org/nbp/common/Braille.java
+++ common/src/org/nbp/common/Braille.java
@@ -1,15 +1,15 @@
 package org.nbp.common;
 
 public abstract class Braille {
-  public final static char UNICODE_ROW = 0X2800;
-  public final static byte UNICODE_DOT_1 =       0X01;
-  public final static byte UNICODE_DOT_2 =       0X02;
-  public final static byte UNICODE_DOT_3 =       0X04;
-  public final static byte UNICODE_DOT_4 =       0X08;
-  public final static byte UNICODE_DOT_5 =       0X10;
-  public final static byte UNICODE_DOT_6 =       0X20;
-  public final static byte UNICODE_DOT_7 =       0X40;
-  public final static byte UNICODE_DOT_8 = (byte)0X80;
+  public final static char UNICODE_ROW   = 0X2800;
+  public final static char UNICODE_DOT_1 = 0X0001;
+  public final static char UNICODE_DOT_2 = 0X0002;
+  public final static char UNICODE_DOT_3 = 0X0004;
+  public final static char UNICODE_DOT_4 = 0X0008;
+  public final static char UNICODE_DOT_5 = 0X0010;
+  public final static char UNICODE_DOT_6 = 0X0020;
+  public final static char UNICODE_DOT_7 = 0X0040;
+  public final static char UNICODE_DOT_8 = 0X0080;
 
   protected Braille () {
   }
