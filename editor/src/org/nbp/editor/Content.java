209120bc1e2d518afbbe22a1f22e65878d2965b9
Dave Mielke <dave@mielke.cc>
2017-02-12 11:49:38 -0500

    Attach mime types to content operations. (dm)

--- editor/src/org/nbp/editor/Content.java
+++ editor/src/org/nbp/editor/Content.java
@@ -37,8 +37,13 @@ public abstract class Content {
   public final static class FormatDescriptor {
     private final OperationsInstantiator operationsInstantiator;
     private final String formatName;
-    private final String[] fileExtensions;
-    private final String selectorLabel;
+
+    private FormatDescriptor (
+      Class<? extends ContentOperations> instantiator, int name
+    ) {
+      operationsInstantiator = new OperationsInstantiator(instantiator);
+      formatName = getString(name);
+    }
 
     public final ContentOperations getOperations () {
       return operationsInstantiator.get();
@@ -48,92 +53,216 @@ public abstract class Content {
       return formatName;
     }
 
+    private final List<String> fileExtensions = new ArrayList<String>();
+    private final List<String> mimeTypes = new ArrayList<String>();
+    private String selectorLabel = null;
+
+    private final void addFileExtension (String extension) {
+      if (fileExtensions.isEmpty()) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(extension);
+
+        sb.append(" [");
+        sb.append(formatName);
+        sb.append(']');
+
+        selectorLabel = sb.toString();
+      }
+
+      fileExtensions.add(extension);
+    }
+
     public final String[] getFileExtensions () {
-      return fileExtensions;
+      return fileExtensions.toArray(new String[fileExtensions.size()]);
     }
 
     public final String getFileExtension () {
-      return getFileExtensions()[0];
+      return fileExtensions.get(0);
+    }
+
+    private final void addMimeType (String type) {
+      mimeTypes.add(type);
+    }
+
+    public final String[] getMimeTypes () {
+      return mimeTypes.toArray(new String[mimeTypes.size()]);
+    }
+
+    public final String getMimeType () {
+      return mimeTypes.get(0);
     }
 
     public final String getSelectorLabel () {
       return selectorLabel;
-    }
-
-    private final String makeSelectorLabel () {
-      StringBuilder sb = new StringBuilder();
-      sb.append(getFileExtension());
-
-      sb.append(" [");
-      sb.append(getFormatName());
-      sb.append(']');
-
-      return sb.toString();
-    }
-
-    private FormatDescriptor (
-      Class<? extends ContentOperations> instantiator,
-      int name, String[] extensions
-    ) {
-      operationsInstantiator = new OperationsInstantiator(instantiator);
-      formatName = getString(name);
-      fileExtensions = extensions;
-      selectorLabel = makeSelectorLabel();
     }
   }
 
   private final static List<FormatDescriptor> formatDescriptors
             = new ArrayList<FormatDescriptor>();
 
-  private final static Map<String, FormatDescriptor> extensionToFormat
-             = new HashMap<String, FormatDescriptor>();
-
-  private static FormatDescriptor addFormat (
-    Class<? extends ContentOperations> instantiator,
-    int name, String... extensions
-  ) {
-    FormatDescriptor format = new FormatDescriptor(instantiator, name, extensions);
-    formatDescriptors.add(format);
-
-    for (String extension : extensions) {
-      extensionToFormat.put(extension, format);
-    }
-
-    return format;
-  }
-
-  private final static FormatDescriptor DEFAULT_FORMAT;
-
-  static {
-    DEFAULT_FORMAT = addFormat(TextOperations.class, R.string.format_name_txt, ".txt");
-
-    addFormat(ASCIIBrailleOperations.class, R.string.format_name_brl, ".brl");
-    addFormat(ASCIIBrailleOperations.class, R.string.format_name_brf, ".brf");
-
-    addFormat(BrailleKeywordOperations.class, R.string.format_name_kwb, ".kwb");
-    addFormat(TextKeywordOperations.class, R.string.format_name_kwt, ".kwt");
-
-    addFormat(ODTOperations.class, R.string.format_name_odt, ".odt");
-    addFormat(RTFOperations.class, R.string.format_name_rtf, ".rtf");
-
-    addFormat(DocOperations.class, R.string.format_name_doc, ".doc");
-    addFormat(DocXOperations.class, R.string.format_name_docx, ".docx");
-    addFormat(DocMOperations.class, R.string.format_name_docm, ".docm");
-
-    addFormat(HTMLOperations.class, R.string.format_name_html, ".html", ".htm");
-    addFormat(MHTMLOperations.class, R.string.format_name_mhtml, ".mhtml", ".mht");
-
-    addFormat(PDFOperations.class, R.string.format_name_pdf, ".pdf");
-    addFormat(PSOperations.class, R.string.format_name_ps, ".ps");
-
-    addFormat(XPSOperations.class, R.string.format_name_xps, ".xps");
-    addFormat(OXPSOperations.class, R.string.format_name_oxps, ".oxps");
-
-    addFormat(EPubOperations.class, R.string.format_name_epub, ".epub");
-  }
-
   public static FormatDescriptor[] getFormatDescriptors () {
     return formatDescriptors.toArray(new FormatDescriptor[formatDescriptors.size()]);
+  }
+
+  private final static Map<String, FormatDescriptor> fileExtensionMap
+             = new HashMap<String, FormatDescriptor>();
+
+  private static void addFileExtension (FormatDescriptor descriptor, String extension) {
+    descriptor.addFileExtension(extension);
+    fileExtensionMap.put(extension, descriptor);
+  }
+
+  private final static Map<String, FormatDescriptor> mimeTypeMap
+             = new HashMap<String, FormatDescriptor>();
+
+  private static void addMimeType (FormatDescriptor descriptor, String type) {
+    descriptor.addMimeType(type);
+    mimeTypeMap.put(type, descriptor);
+  }
+
+  private static FormatDescriptor addFormat (
+    Class<? extends ContentOperations> instantiator, int name
+  ) {
+    FormatDescriptor descriptor = new FormatDescriptor(instantiator, name);
+    formatDescriptors.add(descriptor);
+    return descriptor;
+  }
+
+  private final static FormatDescriptor DEFAULT_FORMAT_DESCRIPTOR;
+
+  static {
+    DEFAULT_FORMAT_DESCRIPTOR = addFormat(TextOperations.class, R.string.format_name_txt);
+    addFileExtension(DEFAULT_FORMAT_DESCRIPTOR, ".txt");
+    addMimeType(DEFAULT_FORMAT_DESCRIPTOR, "text/plain");
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        ASCIIBrailleOperations.class, R.string.format_name_brl
+      );
+
+      addFileExtension(descriptor, ".brl");
+      addFileExtension(descriptor, ".brf");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        DocOperations.class, R.string.format_name_doc
+      );
+
+      addFileExtension(descriptor, ".doc");
+      addMimeType(descriptor, "application/msword");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        DocMOperations.class, R.string.format_name_docm
+      );
+
+      addFileExtension(descriptor, ".docm");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        DocXOperations.class, R.string.format_name_docx
+      );
+
+      addFileExtension(descriptor, ".docx");
+      addMimeType(descriptor, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        EPubOperations.class, R.string.format_name_epub
+      );
+
+      addFileExtension(descriptor, ".epub");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        HTMLOperations.class, R.string.format_name_html
+      );
+
+      addFileExtension(descriptor, ".html");
+      addFileExtension(descriptor, ".htm");
+      addMimeType(descriptor, "text/html");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        BrailleKeywordOperations.class, R.string.format_name_kwb
+      );
+
+      addFileExtension(descriptor, ".kwb");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        TextKeywordOperations.class, R.string.format_name_kwt
+      );
+
+      addFileExtension(descriptor, ".kwt");
+      addMimeType(descriptor, "application/x-kword");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        MHTMLOperations.class, R.string.format_name_mhtml
+      );
+
+      addFileExtension(descriptor, ".mhtml");
+      addFileExtension(descriptor, ".mht");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        ODTOperations.class, R.string.format_name_odt
+      );
+
+      addFileExtension(descriptor, ".odt");
+      addMimeType(descriptor, "application/vnd.oasis.opendocument.text");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        OXPSOperations.class, R.string.format_name_oxps
+      );
+
+      addFileExtension(descriptor, ".oxps");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        PDFOperations.class, R.string.format_name_pdf
+      );
+
+      addFileExtension(descriptor, ".pdf");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        PSOperations.class, R.string.format_name_ps
+      );
+
+      addFileExtension(descriptor, ".ps");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        RTFOperations.class, R.string.format_name_rtf
+      );
+
+      addFileExtension(descriptor, ".rtf");
+      addMimeType(descriptor, "text/rtf");
+    }
+
+    {
+      FormatDescriptor descriptor = addFormat(
+        XPSOperations.class, R.string.format_name_xps
+      );
+
+      addFileExtension(descriptor, ".xps");
+    }
   }
 
   public static String getExtension (File file) {
@@ -150,7 +279,7 @@ public abstract class Content {
   }
 
   public static FormatDescriptor getFormatDescriptor (String extension) {
-    return extensionToFormat.get(extension.toLowerCase());
+    return fileExtensionMap.get(extension.toLowerCase());
   }
 
   public static FormatDescriptor getFormatDescriptor (File file) {
@@ -160,9 +289,9 @@ public abstract class Content {
   }
 
   public static ContentOperations getContentOperations (File file) {
-    FormatDescriptor format = getFormatDescriptor(file);
-    if (format == null) format = DEFAULT_FORMAT;
-    return format.getOperations();
+    FormatDescriptor descriptor = getFormatDescriptor(file);
+    if (descriptor == null) descriptor = DEFAULT_FORMAT_DESCRIPTOR;
+    return descriptor.getOperations();
   }
 
   public static ContentOperations getContentOperations (Uri uri) {
