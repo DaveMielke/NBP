09549ede69776da6dc00f0e5c54748ce40bf56ee
Dave Mielke <dave@mielke.cc>
2017-02-13 02:12:11 -0500

    The log logger now lets the log level be specified. (dm)

--- common/src/org/nbp/common/LogLogger.java
+++ common/src/org/nbp/common/LogLogger.java
@@ -4,15 +4,21 @@ import android.util.Log;
 
 public class LogLogger extends Logger {
   private final String logTag;
+  private final int logLevel;
 
-  public LogLogger (String tag, Iterator iterator) {
+  public LogLogger (String tag, int level, Iterator iterator) {
     super(iterator);
     logTag = tag;
+    logLevel = level;
+  }
+
+  public LogLogger (String tag, Iterator iterator) {
+    this(tag, Log.VERBOSE, iterator);
   }
 
   @Override
   protected final boolean write (String line) {
-    Log.d(logTag, line);
+    Log.println(logLevel, logTag, line);
     return true;
   }
 
