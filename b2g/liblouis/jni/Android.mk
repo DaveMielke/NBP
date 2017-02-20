a2576259494b804f7459f18f32ede711b0f272fa
Dave Mielke <dave@mielke.cc>
2017-02-20 04:35:44 -0500

    Switch to using an external copy of LibLouis. (dm)

--- b2g/liblouis/jni/Android.mk
+++ b2g/liblouis/jni/Android.mk
@@ -3,16 +3,17 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := louis
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/liblouis-c/liblouis
-LOCAL_CFLAGS := -Wall -DTABLESDIR=\"liblouis-c/tables\"
+LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/liblouis-location/liblouis
+LOCAL_CFLAGS := -Wall -DTABLESDIR=\"liblouis-location/tables\"
 LOCAL_LDLIBS := -llog
 
 LOCAL_SRC_FILES := \
-   liblouis-c/liblouis/compileTranslationTable.c \
-   liblouis-c/liblouis/lou_translateString.c \
-   liblouis-c/liblouis/lou_backTranslateString.c \
-   liblouis-c/liblouis/logging.c \
-   liblouis-c/liblouis/wrappers.c \
+   liblouis-location/liblouis/compileTranslationTable.c \
+   liblouis-location/liblouis/lou_translateString.c \
+   liblouis-location/liblouis/lou_backTranslateString.c \
+   liblouis-location/liblouis/pattern.c \
+   liblouis-location/liblouis/logging.c \
+   liblouis-location/liblouis/wrappers.c \
    log.c \
    translation.c \
    louis.c
