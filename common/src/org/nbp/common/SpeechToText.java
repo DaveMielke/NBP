4936e88c4539634fc5ec25bd4d67ffedb0321966
Dave Mielke <dave@mielke.cc>
2017-02-22 18:27:01 -0500

    Implement more of the SpeechToText object. (dm)

--- common/src/org/nbp/common/SpeechToText.java
+++ common/src/org/nbp/common/SpeechToText.java
@@ -1,14 +1,29 @@
 package org.nbp.common;
 
-import android.content.Intent;
-import android.speech.RecognizerIntent;
+import java.util.ArrayList;
 import java.util.Locale;
 
-public class SpeechToText {
-  private final CommonActivity mainActivity;
+import android.util.Log;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 
-  public SpeechToText (CommonActivity activity) {
-    mainActivity = activity;
+public abstract class SpeechToText {
+  private final static String LOG_TAG = SpeechToText.class.getName();
+
+  private SpeechToText () {
+  }
+
+  public static class TextHandler {
+    public TextHandler () {
+    }
+
+    public void handleText (ArrayList<String> text) {
+    }
+
+    public void reportProblem (String problem) {
+      Log.w(LOG_TAG, problem);
+    }
   }
 
   public enum LanguageModel {
@@ -26,66 +41,146 @@ public class SpeechToText {
     }
   }
 
-  private String selectedLanguage = null;
-  private LanguageModel selectedModel = null;
-  private String selectedPrompt = null;
+  public static class Builder {
+    private final CommonActivity mainActivity;
 
-  public final String getLanguage () {
-    return selectedLanguage;
-  }
-
-  public final SpeechToText setLanguage (String language) {
-    selectedLanguage = language;
-    return this;
-  }
-
-  public final SpeechToText setLanguage (Locale locale) {
-    return setLanguage(locale.toString());
-  }
-
-  public final LanguageModel getModel () {
-    return selectedModel;
-  }
-
-  public final SpeechToText setModel (LanguageModel model) {
-    selectedModel = model;
-    return this;
-  }
-
-  public final String getPrompt () {
-    return selectedPrompt;
-  }
-
-  public final SpeechToText setPrompt (String prompt) {
-    selectedPrompt = prompt;
-    return this;
-  }
-
-  public final SpeechToText setPrompt (int prompt) {
-    return setPrompt(mainActivity.getString(prompt));
-  }
-
-  public final boolean start (ActivityResultHandler handler) {
-    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-    {
-      String language = getLanguage();
-      if (language == null) language = Locale.getDefault().toString();
-      intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
+    public Builder (CommonActivity activity) {
+      mainActivity = activity;
     }
 
-    {
-      LanguageModel model = getModel();
-      if (model == null) model = LanguageModel.FREE_FORM;
-      intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, model.getValue());
+    public final CommonActivity getActivity () {
+      return mainActivity;
     }
 
-    {
-      String prompt = getPrompt();
-      if (prompt == null) prompt = "";
-      intent.putExtra(RecognizerIntent.EXTRA_PROMPT, prompt);
+    private String selectedLanguage = null;
+    private LanguageModel selectedModel = null;
+    private String selectedPrompt = null;
+
+    public final String getLanguage () {
+      return selectedLanguage;
     }
 
-    return mainActivity.startRequest(intent, handler);
+    public final Builder setLanguage (String language) {
+      selectedLanguage = language;
+      return this;
+    }
+
+    public final Builder setLanguage (Locale locale) {
+      return setLanguage(locale.toString());
+    }
+
+    public final LanguageModel getModel () {
+      return selectedModel;
+    }
+
+    public final Builder setModel (LanguageModel model) {
+      selectedModel = model;
+      return this;
+    }
+
+    public final String getPrompt () {
+      return selectedPrompt;
+    }
+
+    public final Builder setPrompt (String prompt) {
+      selectedPrompt = prompt;
+      return this;
+    }
+
+    public final Builder setPrompt (int prompt) {
+      return setPrompt(mainActivity.getString(prompt));
+    }
+
+    public final boolean start (ActivityResultHandler handler) {
+      Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+      {
+        String language = getLanguage();
+        if (language == null) language = Locale.getDefault().toString();
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
+      }
+
+      {
+        LanguageModel model = getModel();
+        if (model == null) model = LanguageModel.FREE_FORM;
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, model.getValue());
+      }
+
+      {
+        String prompt = getPrompt();
+        if (prompt == null) prompt = "";
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, prompt);
+      }
+
+      return mainActivity.startRequest(intent, handler);
+    }
+
+    public final boolean start (final TextHandler textHandler) {
+      ActivityResultHandler resultHandler = new ActivityResultHandler() {
+        @Override
+        public void handleActivityResult (int code, Intent intent) {
+          String problem;
+
+          switch (code) {
+            case CommonActivity.RESULT_OK: {
+              if (intent != null) {
+                ArrayList<String> text = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if (text != null) textHandler.handleText(text);
+              }
+             
+              return;
+            }
+
+            case CommonActivity.RESULT_CANCELED:
+              problem = "request cancelled";
+              break;
+
+            case SpeechRecognizer.ERROR_AUDIO:
+              problem = "recording error";
+              break;
+
+            case SpeechRecognizer.ERROR_CLIENT:
+              problem = "client error";
+              break;
+
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+              problem = "not permitted";
+              break;
+
+            case SpeechRecognizer.ERROR_NETWORK:
+              problem = "network error";
+              break;
+
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+              problem = "network timeout";
+              break;
+
+            case SpeechRecognizer.ERROR_NO_MATCH:
+              problem = "no match";
+              break;
+
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+              problem = "service busy";
+              break;
+
+            case SpeechRecognizer.ERROR_SERVER:
+              problem = "server error";
+              break;
+
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+              problem = "no input";
+              break;
+
+            default:
+              problem = "unknown problem";
+              break;
+          }
+
+          textHandler.reportProblem(problem);
+        }
+      };
+
+      return start(resultHandler);
+    }
   }
 }
