6aeb9fd0e590db51e99a357c4b20df9c2fe26f68
Dave Mielke <dave@mielke.cc>
2017-03-18 13:31:15 -0400

    Reorganize the internaitonal unit prefixes. (dm)

---
+++ calculator/src/org/nbp/calculator/conversion/InternationalPrefix.java
@@ -0,0 +1,37 @@
+package org.nbp.calculator.conversion;
+
+public abstract class InternationalPrefix {
+  private InternationalPrefix () {
+  }
+
+  public final static double DECA = 1E1;
+  public final static double HECTO = 1E2;
+  public final static double KILO = 1E3;
+  public final static double MEGA = 1E6;
+  public final static double GIGA = 1E9;
+  public final static double TERA = 1E12;
+  public final static double PETA = 1E15;
+  public final static double EXA = 1E18;
+  public final static double ZETTA = 1E21;
+  public final static double YOTTA = 1E24;
+
+  public final static double DECI = 1E-1;
+  public final static double CENTI = 1E-2;
+  public final static double MILLI = 1E-3;
+  public final static double MICRO = 1E-6;
+  public final static double NANO = 1E-9;
+  public final static double PICO = 1E-12;
+  public final static double FEMTO = 1E-15;
+  public final static double ATTO = 1E-18;
+  public final static double ZEPTO = 1E-21;
+  public final static double YOCTO = 1E-24;
+
+  public final static double KIBI = 0X1P10;
+  public final static double MEBI = 0X1P20;
+  public final static double GIBI = 0X1P30;
+  public final static double TEBI = 0X1P40;
+  public final static double PEBI = 0X1P50;
+  public final static double EXBI = 0X1P60;
+  public final static double ZEBI = 0X1P70;
+  public final static double YOBI = 0X1P80;
+}
--- calculator/src/org/nbp/calculator/conversion/InternationalUnit.java
+++ calculator/src/org/nbp/calculator/conversion/InternationalUnit.java
@@ -12,34 +12,34 @@ public class InternationalUnit extends Unit {
     super(type, names);
   }
 
-  public final Unit YOTTA = newUnit(1E24, "yotta", "Y");
-  public final Unit ZETTA = newUnit(1E21, "zetta", "Z");
-  public final Unit EXA = newUnit(1E18, "exa", "E");
-  public final Unit PETA = newUnit(1E15, "peta", "P");
-  public final Unit TERA = newUnit(1E12, "tera", "T");
-  public final Unit GIGA = newUnit(1E9, "giga", "G");
-  public final Unit MEGA = newUnit(1E6, "mega", "M");
-  public final Unit KILO = newUnit(1E3, "kilo", "k");
-  public final Unit HECTO = newUnit(1E2, "hecto", "h");
-  public final Unit DECA = newUnit(1E1, "deca", "da");
-  public final Unit DEKA = newUnit(1E1, "deka", "dk");
-  public final Unit DECI = newUnit(1E-1, "deci", "d");
-  public final Unit CENTI = newUnit(1E-2, "centi", "c");
-  public final Unit MILLI = newUnit(1E-3, "milli", "m");
-  public final Unit MICRO = newUnit(1E-6, "micro", "u");
-  public final Unit NANO = newUnit(1E-9, "nano", "n");
-  public final Unit PICO = newUnit(1E-12, "pico", "p");
-  public final Unit FEMTO = newUnit(1E-15, "femto", "f");
-  public final Unit ATTO = newUnit(1E-18, "atto", "a");
-  public final Unit ZEPTO = newUnit(1E-21, "zepto", "z");
-  public final Unit YOCTO = newUnit(1E-24, "yocto", "y");
+  public final Unit DECA = newUnit(InternationalPrefix.DECA, "deca", "da");
+  public final Unit HECTO = newUnit(InternationalPrefix.HECTO, "hecto", "h");
+  public final Unit KILO = newUnit(InternationalPrefix.KILO, "kilo", "k");
+  public final Unit MEGA = newUnit(InternationalPrefix.MEGA, "mega", "M");
+  public final Unit GIGA = newUnit(InternationalPrefix.GIGA, "giga", "G");
+  public final Unit TERA = newUnit(InternationalPrefix.TERA, "tera", "T");
+  public final Unit PETA = newUnit(InternationalPrefix.PETA, "peta", "P");
+  public final Unit EXA = newUnit(InternationalPrefix.EXA, "exa", "E");
+  public final Unit ZETTA = newUnit(InternationalPrefix.ZETTA, "zetta", "Z");
+  public final Unit YOTTA = newUnit(InternationalPrefix.YOTTA, "yotta", "Y");
 
-  public final Unit KIBI = newUnit(0X1P10, "kibi", "Ki");
-  public final Unit MEBI = newUnit(0X1P20, "mebi", "Mi");
-  public final Unit GIBI = newUnit(0X1P30, "gibi", "Gi");
-  public final Unit TEBI = newUnit(0X1P40, "tebi", "Ti");
-  public final Unit PEBI = newUnit(0X1P50, "pebi", "Pi");
-  public final Unit EXBI = newUnit(0X1P60, "exbi", "Ei");
-  public final Unit ZEBI = newUnit(0X1P70, "zebi", "Zi");
-  public final Unit YOBI = newUnit(0X1P80, "yobi", "Yi");
+  public final Unit DECI = newUnit(InternationalPrefix.DECI, "deci", "d");
+  public final Unit CENTI = newUnit(InternationalPrefix.CENTI, "centi", "c");
+  public final Unit MILLI = newUnit(InternationalPrefix.MILLI, "milli", "m");
+  public final Unit MICRO = newUnit(InternationalPrefix.MICRO, "micro", "u");
+  public final Unit NANO = newUnit(InternationalPrefix.NANO, "nano", "n");
+  public final Unit PICO = newUnit(InternationalPrefix.PICO, "pico", "p");
+  public final Unit FEMTO = newUnit(InternationalPrefix.FEMTO, "femto", "f");
+  public final Unit ATTO = newUnit(InternationalPrefix.ATTO, "atto", "a");
+  public final Unit ZEPTO = newUnit(InternationalPrefix.ZEPTO, "zepto", "z");
+  public final Unit YOCTO = newUnit(InternationalPrefix.YOCTO, "yocto", "y");
+
+  public final Unit KIBI = newUnit(InternationalPrefix.KIBI, "kibi", "Ki");
+  public final Unit MEBI = newUnit(InternationalPrefix.MEBI, "mebi", "Mi");
+  public final Unit GIBI = newUnit(InternationalPrefix.GIBI, "gibi", "Gi");
+  public final Unit TEBI = newUnit(InternationalPrefix.TEBI, "tebi", "Ti");
+  public final Unit PEBI = newUnit(InternationalPrefix.PEBI, "pebi", "Pi");
+  public final Unit EXBI = newUnit(InternationalPrefix.EXBI, "exbi", "Ei");
+  public final Unit ZEBI = newUnit(InternationalPrefix.ZEBI, "zebi", "Zi");
+  public final Unit YOBI = newUnit(InternationalPrefix.YOBI, "yobi", "Yi");
 }
