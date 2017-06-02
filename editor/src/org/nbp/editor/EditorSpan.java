8b380309c2e2ff800746f3d1478290e908c094fa
Dave Mielke <dave@mielke.cc>
2017-06-02 17:38:19 -0400

    Don't join deletions if they're of insertions made by different reviewers. (dm)

--- editor/src/org/nbp/editor/EditorSpan.java
+++ editor/src/org/nbp/editor/EditorSpan.java
@@ -27,86 +27,11 @@ public abstract class EditorSpan implements DialogFinisher {
     return content.getSpanStart(this);
   }
 
-  private final static void joinRevisions (Editable content) {
-    int length = content.length();
-    int start = 0;
-
-    RevisionSpan previousRevision = null;
-    final int NO_POSITION = -1;
-    int previousStart = NO_POSITION;
-    int previousEnd = NO_POSITION;
-
-    while (start != length) {
-      int end = content.nextSpanTransition(start, length, RevisionSpan.class);
-      RevisionSpan[] nextRevisions = content.getSpans(start, end, RevisionSpan.class);
-
-      if ((nextRevisions != null) && (nextRevisions.length == 1)) {
-        RevisionSpan nextRevision = nextRevisions[0];
-        int nextStart = content.getSpanStart(nextRevision);
-        int nextEnd = content.getSpanEnd(nextRevision);
-        boolean isJoinable = false;
-
-      TEST_JOINABILITY:
-        {
-          if (nextStart != previousEnd) break TEST_JOINABILITY;
-          if (nextRevision.getClass() != previousRevision.getClass()) break TEST_JOINABILITY;
-
-          {
-            String name = nextRevision.getReviewerName();
-            if (name == null) break TEST_JOINABILITY;
-            if (!name.equals(previousRevision.getReviewerName())) break TEST_JOINABILITY;
-          }
-
-          {
-            Date nextTime = nextRevision.getReviewTime();
-            if (nextTime == null) break TEST_JOINABILITY;
-
-            Date previousTime = previousRevision.getReviewTime();
-            if (previousTime == null) break TEST_JOINABILITY;
-
-            long timeDifference = nextTime.getTime() - previousTime.getTime();
-            if (Math.abs(timeDifference) > ApplicationParameters.REVISION_JOIN_MILLISECONDS) break TEST_JOINABILITY;
-          }
-
-          isJoinable = true;
-        }
-
-        if (isJoinable) {
-          {
-            Date time = nextRevision.getReviewTime();
-
-            if (previousRevision.getReviewTime().compareTo(time) < 0) {
-              previousRevision.setReviewTime(time);
-            }
-          }
-
-          content.removeSpan(nextRevision);
-          content.setSpan(
-            previousRevision, previousStart, nextEnd,
-            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-          );
-
-          previousEnd = content.getSpanEnd(previousRevision);
-        } else {
-          previousRevision = nextRevision;
-          previousStart = nextStart;
-          previousEnd = nextEnd;
-        }
-      } else {
-        previousRevision = null;
-        previousStart = NO_POSITION;
-        previousEnd = NO_POSITION;
-      }
-
-      start = end;
-    }
-  }
-
   public void finishSpan (Editable content) {
   }
 
   public final static void finishSpans (Editable content) {
-    joinRevisions(content);
+    Revisions.joinRevisions(content);
 
     for (EditorSpan span : content.getSpans(0, content.length(), EditorSpan.class)) {
       span.finishSpan(content);
---
+++ editor/src/org/nbp/editor/Revisions.java
@@ -0,0 +1,108 @@
+package org.nbp.editor;
+
+import java.util.Date;
+
+import android.text.Editable;
+import android.text.Spanned;
+
+public abstract class Revisions {
+  private Revisions () {
+  }
+
+  private final static boolean isSameReviewer (RevisionSpan revision1, RevisionSpan revision2) {
+    String name = revision1.getReviewerName();
+    if (name == null) return false;
+    return name.equals(revision2.getReviewerName());
+  }
+
+  private final static boolean canJoinRevisions (RevisionSpan revision1, RevisionSpan revision2) {
+    if (revision1.getClass() != revision2.getClass()) return false;
+    if (!isSameReviewer(revision1, revision2)) return false;
+
+    {
+      Date time1 = revision1.getReviewTime();
+      if (time1 == null) return false;
+
+      Date time2 = revision2.getReviewTime();
+      if (time2 == null) return false;
+
+      long timeDifference = time2.getTime() - time1.getTime();
+      if (Math.abs(timeDifference) > ApplicationParameters.REVISION_JOIN_MILLISECONDS) return false;
+    }
+
+    if (revision1 instanceof DeletionSpan) {
+      DeletionSpan deletion1 = (DeletionSpan)revision1;
+      DeletionSpan deletion2 = (DeletionSpan)revision2;
+
+      InsertionSpan insertion1 = deletion1.getInsertion();
+      InsertionSpan insertion2 = deletion2.getInsertion();
+
+      boolean haveInsertion1 = insertion1 != null;
+      boolean haveInsertion2 = insertion2 != null;
+
+      if (haveInsertion1 || haveInsertion2) {
+        if (!haveInsertion1) return false;
+        if (!haveInsertion2) return false;
+        if (!isSameReviewer(insertion1, insertion2)) return false;
+      }
+    }
+
+    return true;
+  }
+
+  public final static void joinRevisions (Editable content) {
+    int length = content.length();
+    int start = 0;
+
+    RevisionSpan previousRevision = null;
+    final int NO_POSITION = -1;
+    int previousStart = NO_POSITION;
+    int previousEnd = NO_POSITION;
+
+    while (start != length) {
+      int end = content.nextSpanTransition(start, length, RevisionSpan.class);
+      RevisionSpan[] nextRevisions = content.getSpans(start, end, RevisionSpan.class);
+
+      if ((nextRevisions != null) && (nextRevisions.length == 1)) {
+        RevisionSpan nextRevision = nextRevisions[0];
+        int nextStart = content.getSpanStart(nextRevision);
+        int nextEnd = content.getSpanEnd(nextRevision);
+        boolean join = false;
+
+        if (nextStart == previousEnd) {
+          if (canJoinRevisions(previousRevision, nextRevision)) {
+            join = true;
+          }
+        }
+
+        if (join) {
+          {
+            Date time = nextRevision.getReviewTime();
+
+            if (previousRevision.getReviewTime().compareTo(time) < 0) {
+              previousRevision.setReviewTime(time);
+            }
+          }
+
+          content.removeSpan(nextRevision);
+          content.setSpan(
+            previousRevision, previousStart, nextEnd,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+          );
+
+          previousEnd = content.getSpanEnd(previousRevision);
+        } else {
+          previousRevision = nextRevision;
+          previousStart = nextStart;
+          previousEnd = nextEnd;
+        }
+      } else {
+        previousRevision = null;
+        previousStart = NO_POSITION;
+        previousEnd = NO_POSITION;
+      }
+
+      start = end;
+    }
+  }
+}
