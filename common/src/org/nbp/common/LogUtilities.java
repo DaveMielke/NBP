51051055ff51612c9d1ca2f675e553a76583e666
Dave Mielke <dave@mielke.cc>
2017-06-03 10:08:28 -0400

    logSpans() should be a common method (not just for the editor). (dm)

--- common/src/org/nbp/common/LogUtilities.java
+++ common/src/org/nbp/common/LogUtilities.java
@@ -1,17 +1,59 @@
 package org.nbp.common;
 
 import android.util.Log;
+import android.text.Spanned;
 
 public abstract class LogUtilities {
   private final static String LOG_TAG = LogUtilities.class.getName();
 
-  public final static void log (CharSequence name, CharSequence string) {
+  protected void LogUtilities () {
+  }
+
+  public final static void logText (CharSequence name, CharSequence text) {
     Log.d(LOG_TAG, String.format(
       "%s: %d/%s/",
-      name, string.length(), string
+      name, text.length(), text
     ));
   }
 
-  protected void LogUtilities () {
+  public final static void logSpans (CharSequence text, int start, int end) {
+    StringBuilder sb = new StringBuilder("spans:");
+
+    sb.append(' ');
+    sb.append(text.getClass().getName());
+
+    sb.append(' ');
+    sb.append(start);
+    sb.append("..");
+    sb.append(end);
+
+    sb.append(' ');
+    sb.append('"');
+    sb.append(text.subSequence(start, end));
+    sb.append('"');
+
+    if (text instanceof Spanned) {
+      Spanned content = (Spanned)text;
+
+      Object[] spans = content.getSpans(start, end, Object.class);
+      sb.append(' ');
+      sb.append(spans.length);
+
+      for (Object span : spans) {
+        sb.append(' ');
+        sb.append(span.getClass().getName());
+
+        sb.append(' ');
+        sb.append(content.getSpanStart(span));
+        sb.append("..");
+        sb.append(content.getSpanEnd(span));
+      }
+    }
+
+    Log.d(LOG_TAG, sb.toString());
+  }
+
+  public final static void logSpans (CharSequence text) {
+    logSpans(text, 0, text.length());
   }
 }
--- editor/src/org/nbp/editor/Spans.java
+++ editor/src/org/nbp/editor/Spans.java
@@ -24,47 +24,6 @@ public abstract class Spans {
   private Spans () {
   }
 
-  public final static void logSpans (CharSequence text, int start, int end) {
-    StringBuilder sb = new StringBuilder("spans:");
-
-    sb.append(' ');
-    sb.append(text.getClass().getName());
-
-    sb.append(' ');
-    sb.append(start);
-    sb.append("..");
-    sb.append(end);
-
-    sb.append(' ');
-    sb.append('"');
-    sb.append(text.subSequence(start, end));
-    sb.append('"');
-
-    if (text instanceof Spanned) {
-      Spanned content = (Spanned)text;
-
-      Object[] spans = content.getSpans(start, end, Object.class);
-      sb.append(' ');
-      sb.append(spans.length);
-
-      for (Object span : spans) {
-        sb.append(' ');
-        sb.append(span.getClass().getName());
-
-        sb.append(' ');
-        sb.append(content.getSpanStart(span));
-        sb.append("..");
-        sb.append(content.getSpanEnd(span));
-      }
-    }
-
-    Log.d(LOG_TAG, sb.toString());
-  }
-
-  public final static void logSpans (CharSequence text) {
-    logSpans(text, 0, text.length());
-  }
-
   private final static String PROPERTY_PREFIX = "-";
 
   private final static void logInvalidSavedValue (String value, String what) {
