5d725555a87e469e126f57eb321e5acf83c7865f
Dave Mielke <dave@mielke.cc>
2017-06-08 16:39:13 -0400

    Add the open submenu action for the spelling correction submenu. (dm)

---
+++ editor/src/org/nbp/editor/menu/spell/OpenSubmenu.java
@@ -0,0 +1,15 @@
+package org.nbp.editor.menu.spell;
+import org.nbp.editor.*;
+
+import android.view.Menu;
+
+public class OpenSubmenu extends MenuAction {
+  public OpenSubmenu (EditorActivity editor) {
+    super(editor);
+  }
+
+  @Override
+  public void prepareMenu (Menu menu) {
+    super.prepareMenu(menu);
+  }
+}
