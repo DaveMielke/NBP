9a7686c23ad55584198e9f8038b03c8bf222a9da
Dave Mielke <dave@mielke.cc>
2017-07-20 00:22:45 -0400

    Split converting an address to a string and logging it. (dm)

--- compass/src/org/nbp/compass/LocationUtilities.java
+++ compass/src/org/nbp/compass/LocationUtilities.java
@@ -9,7 +9,7 @@ public abstract class LocationUtilities {
   private LocationUtilities () {
   }
 
-  private final static void log (StringBuilder sb, String label, String value) {
+  private final static void append (StringBuilder sb, String label, String value) {
     if ((value != null) && !value.isEmpty()) {
       sb.append(' ');
       sb.append(label);
@@ -18,42 +18,44 @@ public abstract class LocationUtilities {
     }
   }
 
-  private final static void log (StringBuilder sb, String label, double value) {
-    log(sb, label, Double.toString(value));
+  public final static String toString (Address address) {
+    StringBuilder sb = new StringBuilder("address");
+
+    sb.append(" [");
+    if (address.hasLatitude()) sb.append(address.getLatitude());
+    sb.append(',');
+    if (address.hasLongitude()) sb.append(address.getLongitude());
+    sb.append("]:");
+
+    append(sb, "CC", address.getCountryCode());
+    append(sb, "CN", address.getCountryName());
+    append(sb, "Adm", address.getAdminArea());
+    append(sb, "SubAdm", address.getSubAdminArea());
+    append(sb, "Loc", address.getLocality());
+    append(sb, "SubLoc", address.getSubLocality());
+    append(sb, "Ftr", address.getFeatureName());
+    append(sb, "Thor", address.getThoroughfare());
+    append(sb, "SubThor", address.getSubThoroughfare());
+    append(sb, "PC", address.getPostalCode());
+    append(sb, "Prem", address.getPremises());
+    append(sb, "Phone", address.getPhone());
+    append(sb, "URL", address.getUrl());
+
+    {
+      int last = address.getMaxAddressLineIndex();
+      int current = 0;
+
+      while (current <= last) {
+        append(sb, ("Line" + current), address.getAddressLine(current++));
+      }
+    }
+
+    return sb.toString();
   }
 
   public final static void log (Address address) {
     if (ApplicationParameters.LOG_ADDRESSES) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("address:");
-
-      log(sb, "CC", address.getCountryCode());
-      log(sb, "CN", address.getCountryName());
-      log(sb, "Adm", address.getAdminArea());
-      log(sb, "SubAdm", address.getSubAdminArea());
-      log(sb, "Loc", address.getLocality());
-      log(sb, "SubLoc", address.getSubLocality());
-      log(sb, "Ftr", address.getFeatureName());
-      log(sb, "Thor", address.getThoroughfare());
-      log(sb, "SubThor", address.getSubThoroughfare());
-      log(sb, "PC", address.getPostalCode());
-      log(sb, "Prem", address.getPremises());
-      log(sb, "Phone", address.getPhone());
-      log(sb, "URL", address.getUrl());
-
-      if (address.hasLatitude()) log(sb, "Lat", address.getLatitude());
-      if (address.hasLongitude()) log(sb, "Lng", address.getLongitude());
-
-      {
-        int last = address.getMaxAddressLineIndex();
-        int current = 0;
-
-        while (current <= last) {
-          log(sb, ("Line" + current), address.getAddressLine(current++));
-        }
-      }
-
-      Log.d(LOG_TAG, sb.toString());
+      Log.d(LOG_TAG, toString(address));
     }
   }
 
