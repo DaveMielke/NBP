c3fc7a4b1e77548829066d7751dc25214ead3a63
Dave Mielke <dave@mielke.cc>
2017-09-01 07:41:37 -0400

    Add start/stop logging to navigation monitors. (dm)

--- navigator/src/org/nbp/navigator/NavigationMonitor.java
+++ navigator/src/org/nbp/navigator/NavigationMonitor.java
@@ -1,8 +1,10 @@
 package org.nbp.navigator;
 
-import android.location.Location;
+import android.util.Log;
 
 public abstract class NavigationMonitor {
+  private final String LOG_TAG = getClass().getName();
+
   protected NavigationMonitor () {
   }
 
@@ -24,10 +26,14 @@ public abstract class NavigationMonitor {
 
   public final void start () {
     if (state != MonitorState.STARTED) {
+      Log.d(LOG_TAG, "starting");
+
       if (startProvider()) {
         state = MonitorState.STARTED;
+        Log.d(LOG_TAG, "started");
       } else {
         state = MonitorState.FAILED;
+        Log.w(LOG_TAG, "failed");
       }
     }
   }
@@ -35,9 +41,11 @@ public abstract class NavigationMonitor {
   public final void stop () {
     switch (state) {
       case STARTED:
+        Log.d(LOG_TAG, "stopping");
         stopProvider();
       case FAILED:
         state = MonitorState.STOPPED;
+        Log.d(LOG_TAG, "stopped");
       case STOPPED:
         break;
     }
--- navigator/src/org/nbp/navigator/OrientationMonitor.java
+++ navigator/src/org/nbp/navigator/OrientationMonitor.java
@@ -3,7 +3,11 @@ package org.nbp.navigator;
 import java.util.Set;
 import java.util.HashSet;
 
+import android.util.Log;
+
 public abstract class OrientationMonitor extends NavigationMonitor {
+  private final static String LOG_TAG = OrientationMonitor.class.getName();
+
   protected OrientationMonitor () {
     super();
   }
@@ -14,6 +18,10 @@ public abstract class OrientationMonitor extends NavigationMonitor {
 
   public enum Reason {
     NAVIGATION_ACTIVITY, LOCATION_MONITOR;
+
+    public final void log (String action) {
+      Log.d(LOG_TAG, String.format("%s for %s", action, name()));
+    }
   }
 
   private final static OrientationMonitor orientationMonitor = new SensorOrientationMonitor();
@@ -28,10 +36,12 @@ public abstract class OrientationMonitor extends NavigationMonitor {
   }
 
   public final static void start (Reason reason) {
+    reason.log("start");
     if (currentReasons.add(reason)) applyCurrentReasons();
   }
 
   public final static void stop (Reason reason) {
+    reason.log("stop");
     if (currentReasons.remove(reason)) applyCurrentReasons();
   }
 }
