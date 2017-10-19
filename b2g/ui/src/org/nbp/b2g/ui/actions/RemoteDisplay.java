5a546d6c8f52b21bd8444c6cb210c9b120b98ab6
Dave Mielke <dave@mielke.cc>
2017-10-19 11:32:38 -0400

    Report but don't go to the remote display if it's off. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/actions/RemoteDisplay.java
+++ b2g/ui/src/org/nbp/b2g/ui/actions/RemoteDisplay.java
@@ -1,10 +1,19 @@
 package org.nbp.b2g.ui.actions;
 import org.nbp.b2g.ui.*;
 
+import org.nbp.common.controls.BooleanControl;
+
 public class RemoteDisplay extends Action {
   @Override
   public boolean performAction () {
-    Endpoints.setRemoteEndpoint();
+    BooleanControl control = Controls.remoteDisplay;
+
+    if (control.getBooleanValue()) {
+      Endpoints.setRemoteEndpoint();
+    } else {
+      control.confirmValue();
+    }
+
     return true;
   }
 
