932199bae40d4eccc3c1a490597c20a1b354ad72
Dave Mielke <dave@mielke.cc>
2017-11-22 18:21:38 -0500

    Add Translator.makeInputOffsts(). (dm)

--- liblouis/src/org/liblouis/Translator.java
+++ liblouis/src/org/liblouis/Translator.java
@@ -5,6 +5,51 @@ public abstract class Translator extends TranslationComponent {
     super();
   }
 
+  public final static int makeInputOffsets (int[] inputOffsets, int[] outputOffsets) {
+    int inputLength = outputOffsets.length - 1;
+    int outputLength = Math.min(inputOffsets.length-1, outputOffsets[inputLength]);
+
+    int inputIndex = 0;
+    int outputIndex = 0;
+
+    int inputOffset = 0;
+    int outputOffset;
+
+    while (true) {
+      outputOffset = outputOffsets[inputIndex];
+      if (inputIndex == inputLength) break;
+      if (outputOffset >= outputLength) break;
+
+      while (outputIndex < outputOffset) {
+        inputOffsets[outputIndex++] = inputOffset;
+      }
+
+      inputOffset = inputIndex++;
+    }
+
+    if (outputOffset > outputLength) {
+      if (inputIndex == 0) {
+        outputOffset = 0;
+      } else {
+        outputOffset = outputOffsets[--inputIndex];
+
+        while (inputIndex > 0) {
+          if (outputOffsets[--inputIndex] != outputOffset) {
+            inputIndex += 1;
+            break;
+          }
+        }
+      }
+    }
+
+    while (outputIndex < outputOffset) {
+      inputOffsets[outputIndex++] = inputOffset;
+    }
+
+    inputOffsets[outputIndex] = inputIndex;
+    return inputIndex;
+  }
+
   public abstract boolean translate (
     CharSequence inputBuffer, char[] outputBuffer,
     int[] outputOffsets, int[] inputOffsets,
