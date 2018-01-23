145d6246136fef5e0b4d380fc1820d19906505d0
Dave Mielke <dave@mielke.cc>
2018-01-22 22:25:02 -0500

    Correct some of the character definition comments. (dm)

--- common/src/org/nbp/common/CharacterUtilities.java
+++ common/src/org/nbp/common/CharacterUtilities.java
@@ -3,14 +3,14 @@ package org.nbp.common;
 public abstract class CharacterUtilities {
   // ASCII control characters
   public final static char CHAR_NUL = 0X0000; // null
-  public final static char CHAR_SOH = 0X0001; // start of header
+  public final static char CHAR_SOH = 0X0001; // start of heading
   public final static char CHAR_STX = 0X0002; // start of text
   public final static char CHAR_ETX = 0X0003; // end of text
   public final static char CHAR_EOT = 0X0004; // end of transmission
   public final static char CHAR_ENQ = 0X0005; // enquiry
-  public final static char CHAR_ACK = 0X0006; // acknowledgement (positive)
+  public final static char CHAR_ACK = 0X0006; // (positive) acknowledge
   public final static char CHAR_BEL = 0X0007; // bell
-  public final static char CHAR_BS  = 0X0008; // back space
+  public final static char CHAR_BS  = 0X0008; // backspace
   public final static char CHAR_HT  = 0X0009; // horizontal tab
   public final static char CHAR_LF  = 0X000A; // line feed (new line)
   public final static char CHAR_VT  = 0X000B; // vertical tab
@@ -19,11 +19,11 @@ public abstract class CharacterUtilities {
   public final static char CHAR_SO  = 0X000E; // shift out
   public final static char CHAR_SI  = 0X000F; // shift in
   public final static char CHAR_DLE = 0X0010; // data link escape
-  public final static char CHAR_DC1 = 0X0011; // direct control 1 (X-on)
-  public final static char CHAR_DC2 = 0X0012; // direct control 2
-  public final static char CHAR_DC3 = 0X0013; // direct control 3 (X-off)
-  public final static char CHAR_DC4 = 0X0014; // direct control 4
-  public final static char CHAR_NAK = 0X0015; // negative acknowledgement
+  public final static char CHAR_DC1 = 0X0011; // device control 1 (X-on)
+  public final static char CHAR_DC2 = 0X0012; // device control 2
+  public final static char CHAR_DC3 = 0X0013; // device control 3 (X-off)
+  public final static char CHAR_DC4 = 0X0014; // device control 4
+  public final static char CHAR_NAK = 0X0015; // negative acknowledge
   public final static char CHAR_SYN = 0X0016; // synchronous idle
   public final static char CHAR_ETB = 0X0017; // end of transmission block
   public final static char CHAR_CAN = 0X0018; // cancel
