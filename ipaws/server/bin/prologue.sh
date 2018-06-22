d1abfb91f0d9878f8e3b7f6f5def54a77a318929
Dave Mielke <dave@mielke.cc>
2018-06-22 13:48:49 -0400

    Add support for ipaws.conf. (dm)

--- ipaws/server/bin/prologue.sh
+++ ipaws/server/bin/prologue.sh
@@ -8,7 +8,13 @@ readonly programDirectory="$(realpath "${programDirectory}")"
 
 readonly serverDirectory="${programDirectory%/*}"
 readonly defaultConfigurationDirectory="${serverDirectory}/etc"
+readonly configurationFileName="ipaws.conf"
+
 readonly defaultDataDirectory="${serverDirectory}/var"
+readonly alertsFilePrefix="IPAWS@"
+readonly alertsFileExtension="xml"
+readonly errorExtension="html"
+readonly defaultExtension="txt"
 
 programMessage() {
    local message="${1}"
@@ -88,6 +94,41 @@ showUsageSummary() {
 
 handlePositionalArguments() {
    [ "${#}" -eq 0 ] || syntaxError "too many positional arguments"
+}
+
+processConfigurationFile() {
+   local directory="${1}"
+
+   declare -g -A configuredProperties
+   local file="${directory}/${configurationFileName}"
+   [ -f "${file}" ] && [ -r "${file}" ] || return 0
+   local line number=0
+
+   while read line
+   do
+      let number+=1
+      set -- ${line%%#*}
+      [ "${#}" -eq 0 ] && continue
+
+      local name="${1}"
+      local value="${2}"
+
+      if [ "${#}" -eq 2 ]
+      then
+         configuredProperties["${name}"]="${value}"
+      else
+         local problem
+
+         if [ "${#}" -eq 1 ]
+         then
+            problem="missing value"
+         else
+            problem="excess data"
+         fi
+
+         programMessage "${problem}: ${*}: ${file}[${number}]"
+      fi
+   done <"${file}"
 }
 
 xpathGet() {
