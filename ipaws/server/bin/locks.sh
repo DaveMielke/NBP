3b911074edea23790c84e2f5e196d815eee5e4dc
Dave Mielke <dave@mielke.cc>
2018-06-23 11:18:09 -0400

    Downgrade more ipaws logs to debug. (dm)

--- ipaws/server/bin/locks.sh
+++ ipaws/server/bin/locks.sh
@@ -2,8 +2,8 @@ acquireExclusiveLock() {
    local name="${1}"
 
    exec 9>>"${dataDirectory}/${name}.lock"
-   logInfo "acquiring lock: ${name}"
+   logDebug "acquiring lock: ${name}"
    flock --nonblock --exclusive 9
-   logInfo "lock acquired: ${name}"
+   logDebug "lock acquired: ${name}"
 } && readonly -f acquireExclusiveLock
 
