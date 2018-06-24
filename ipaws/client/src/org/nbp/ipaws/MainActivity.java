e614a2aac252714cba7195de53932ab25d1ef1fe
Dave Mielke <dave@mielke.cc>
2018-06-23 21:46:53 -0400

    Add the base for the ipaws client. (dm)

---
+++ ipaws/client/src/org/nbp/ipaws/MainActivity.java
@@ -0,0 +1,12 @@
+package org.nbp.ipaws;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+  @Override
+  public void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
