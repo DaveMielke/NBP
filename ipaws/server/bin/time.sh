26cbdb1ea602e69cc1045c7a2febe0b0b2e164cf
Dave Mielke <dave@mielke.cc>
2018-06-24 20:20:22 -0400

    Initial implementation of ipaws-filter. (dm)

--- ipaws/server/bin/time.sh
+++ ipaws/server/bin/time.sh
@@ -1,13 +1,17 @@
 readonly utcTimeFormat="%Y-%m-%dT%H:%M:%SZ"
 readonly utcTimeGlob="[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]Z"
 
-utcCurrentTime() {
-   date -u +"${utcTimeFormat}"
-} && readonly -f utcCurrentTime
-
-utcFormatTime() {
+utcTime() {
    local time="${1}"
 
+   [ -n "${time}" ] || time="now"
    date -u -d "${time}" +"${utcTimeFormat}"
-} && readonly -f utcFormatTime
+} && readonly -f utcTime
+
+epochSeconds() {
+   local time="${1}"
+
+   [ -n "${time}" ] || time="now"
+   date -d "${time}" +"%s"
+} && readonly -f epochSeconds
 
