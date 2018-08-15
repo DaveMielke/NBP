a4446a9609a088e79e61ded6afd6b1eab25deb45
Dave Mielke <dave@mielke.cc>
2018-08-15 04:33:59 -0400

    Add the getStates command. (dm)

---
+++ ipaws/server/bin/ipaws-session-getStates.cmd
@@ -0,0 +1,25 @@
+includeScriptLibraries sql
+
+ipawsSession_getStates() {
+   local response="states"
+   local -A result
+
+   sqlEvaluate result "
+   select abbr, name from state order by name;
+   "
+
+   local count="${result["count"]}"
+   local index=0
+
+   while ((index < count))
+   do
+      ((index)) && response+=","
+      let index+=1
+
+      response+=" ${result["${index},abbr"]}"
+      response+=" ${result["${index},name"]}"
+   done
+
+   echo "${response}"
+}
+
