e766833fbcef02985608ab609b483ab730be715e
Dave Mielke <dave@mielke.cc>
2018-08-16 03:56:35 -0400

    Add the setAreas client command. (dm)

---
+++ ipaws/server/bin/ipaws-session-setAreas.cmd
@@ -0,0 +1,31 @@
+includeScriptLibraries sql
+
+ipawsSession_setAreas() {
+   local -A areas
+   areas["000000"]=1
+   local area
+
+   for area
+   do
+      [[ "${area}" =~ ^[0-9]{6}$ ]] || continue
+      areas["${area}"]=1
+
+      area="0${area#?}"
+      areas["${area}"]=1
+
+      area="${area%???}000"
+      areas["${area}"]=1
+   done
+
+   local command="begin transaction;"
+   command+=" delete from areas where client='${clientReference}';"
+
+   for area in "${!areas[@]}"
+   do
+      command+=" insert into areas (client, SAME) values ('${clientReference}', '${area}');"
+   done
+
+   command+=" commit;"
+   sqlExecute "${command}"
+}
+
