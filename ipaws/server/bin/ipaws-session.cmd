017a858f32eb3e0f114fa6ad8de9205f0850714c
Dave Mielke <dave@mielke.cc>
2018-08-23 13:50:27 -0400

    More reorganization of the ipaws-specific shell functions. (dm)

--- ipaws/server/bin/ipaws-session.cmd
+++ ipaws/server/bin/ipaws-session.cmd
@@ -22,6 +22,17 @@ ipawsPrepareClientTables() {
    sqlExecute "${command}"
 } && readonly -f ipawsPrepareClientTables
 
+ipawsSyncAlerts() {
+   set -- *".${alertFileExtension}"
+   local file
+
+   for file
+   do
+      local identifier="${file%.*}"
+      ipawsAlertAdded "${identifier}"
+   done
+} && readonly -f ipawsSyncAlerts
+
 ipawsMonitorAlerts() {
    coproc inotify {
       exec inotifywait --quiet --monitor \
@@ -36,13 +47,8 @@ ipawsMonitorAlerts() {
    eval exec "9<&${inotify[0]}"
 
    (
+      ipawsSyncAlerts
       local time file events
-      set -- *".${alertFileExtension}"
-
-      for file
-      do
-         ipawsAlertAdded "${file%.*}"
-      done
 
       while read -u 9 -r time file events
       do
@@ -73,22 +79,9 @@ ipawsAlertAdded() {
    local identifier="${1}"
 
    ipawsIsSentAlert "${identifier}" || {
-      local file="${identifier}.${alertFileExtension}"
-      local areas="$(capGetAlertProperty "${file}" SQL.areas)"
-      [ -n "${areas}" ] || return 0
-
-      local count
-      sqlCount count requested_areas "client='${clientReference}' and SAME in (${areas})"
-      [ "${count}" -eq 0 ] && return 0
-
-      logInfo "sending alert: ${clientReference} ${identifier}:"
-      beginClientResponse
-      echo "beginAlert ${identifier}"
-      cat "${file}"
-      echo "endAlert ${identifier}"
-      endClientResponse
-
-      ipawsInsertSentAlert "${identifier}"
+      ipawsIsRequestedAlert "${identifier}" && {
+         ipawsSendAlert "${identifier}"
+      }
    }
 } && readonly -f ipawsAlertAdded
 
@@ -101,6 +94,32 @@ ipawsAlertRemoved() {
       ipawsDeleteSentAlert "${identifier}"
    }
 } && readonly -f ipawsAlertRemoved
+
+ipawsSendAlert() {
+   local identifier="${1}"
+
+   logInfo "sending alert: ${clientReference} ${identifier}:"
+   beginClientResponse
+   echo "beginAlert ${identifier}"
+   cat "${identifier}.${alertFileExtension}"
+   echo "endAlert ${identifier}"
+   endClientResponse
+   ipawsInsertSentAlert "${identifier}"
+} && readonly -f ipawsSendAlert
+
+ipawsIsRequestedAlert() {
+   local identifier="${1}"
+
+   local file="${identifier}.${alertFileExtension}"
+   local areas="$(capGetAlertProperty "${file}" SQL.areas)"
+   [ -n "${areas}" ] || return 1
+
+   local count
+   sqlCount count requested_areas "client='${clientReference}' and SAME in (${areas})"
+   [ "${count}" -eq 0 ] && return 1
+
+   return 0
+} && readonly -f ipawsIsRequestedAlert
 
 ipawsIsSentAlert() {
    local identifier="${1}"
