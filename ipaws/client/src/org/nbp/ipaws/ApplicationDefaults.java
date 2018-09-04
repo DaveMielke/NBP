86e1723c569b935d0ff97a3d68374d8a2e872572
Dave Mielke <dave@mielke.cc>
2018-09-04 11:51:56 -0400

    Use echozio (not brltty) as the secondary ipaws server. (dm)

--- ipaws/client/src/org/nbp/ipaws/ApplicationDefaults.java
+++ ipaws/client/src/org/nbp/ipaws/ApplicationDefaults.java
@@ -7,5 +7,5 @@ public abstract class ApplicationDefaults {
   public final static boolean ALERT_MONITOR = true;
 
   public final static String PRIMARY_SERVER = "";
-  public final static String SECONDARY_SERVER = "brltty.app";
+  public final static String SECONDARY_SERVER = "echozio.ca";
 }
