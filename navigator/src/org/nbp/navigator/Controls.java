9280667101dc6224fd9ea5f32b8e58e3dcf4ecc1
Dave Mielke <dave@mielke.cc>
2018-10-24 18:06:56 -0400

    The navigator's settings should be restored in dependency order. (dm)

--- navigator/src/org/nbp/navigator/Controls.java
+++ navigator/src/org/nbp/navigator/Controls.java
@@ -9,55 +9,37 @@ public abstract class Controls {
 
   // The unit control objects need to be constructed first because
   // formatting the values of some of the other controls relies on them.
+  //
+  // unit settings
   public final static DistanceUnitControl distanceUnit = new DistanceUnitControl();
   public final static SpeedUnitControl speedUnit = new SpeedUnitControl();
   public final static AngleUnitControl angleUnit = new AngleUnitControl();
   public final static RelativeDirectionControl relativeDirection = new RelativeDirectionControl();
 
+  // location settings
   public final static AnnounceLocationControl announceLocation = new AnnounceLocationControl();
   public final static LocationMonitorControl locationMonitor = new LocationMonitorControl();
   public final static LocationRadiusControl locationRadius = new LocationRadiusControl();
   public final static UpdateIntervalControl updateInterval = new UpdateIntervalControl();
 
+  // device settings
   public final static ScreenOrientationControl screenOrientation = new ScreenOrientationControl();
 
+  // speech settings
   public final static SpeechVolumeControl speechVolume = new SpeechVolumeControl();
   public final static SpeechRateControl speechRate = new SpeechRateControl();
   public final static SpeechPitchControl speechPitch = new SpeechPitchControl();
   public final static SpeechBalanceControl speechBalance = new SpeechBalanceControl();
 
+  // developer settings
   public final static LogGeocodingControl logGeocoding = new LogGeocodingControl();
   public final static LogSensorsControl logSensors = new LogSensorsControl();
   public final static LocationProviderControl locationProvider = new LocationProviderControl();
 
-  public final static Control[] ALL = new Control[] {
-    // The unit settings need to be restored first because
-    // restoring the values of some of the other settings relies on them.
-    distanceUnit,
-    speedUnit,
-    angleUnit,
-    relativeDirection,
+  public final static Control[] inCreationOrder = Control.getControlsInCreationOrder();
+  public final static Control[] inRestoreOrder = Control.getControlsInRestoreOrder();
 
-    announceLocation,
-    locationMonitor,
-    locationRadius,
-    updateInterval,
-
-    screenOrientation,
-
-    speechVolume,
-    speechRate,
-    speechPitch,
-    speechBalance,
-
-    logGeocoding,
-    logSensors,
-    locationProvider
-  };
-
-  public final static void restore () {
-    for (Control control : ALL) {
-      control.restoreCurrentValue();
-    }
+  public static void restore () {
+    Control.restoreCurrentValues(inRestoreOrder);
   }
 }
