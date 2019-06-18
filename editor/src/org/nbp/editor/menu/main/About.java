85641841cd0da517312f83f0c0e9b08cfc8cf4f0
Dave Mielke <dave@mielke.cc>
2019-06-18 13:13:39 -0400

    The editor's about screen should include the liblouis version. (dm)

--- editor/src/org/nbp/editor/menu/main/About.java
+++ editor/src/org/nbp/editor/menu/main/About.java
@@ -4,6 +4,8 @@ import org.nbp.editor.*;
 import org.nbp.common.DialogFinisher;
 import org.nbp.common.DialogHelper;
 
+import org.liblouis.Louis;
+
 public class About extends EditorAction implements DialogFinisher {
   public About (EditorActivity editor) {
     super(editor);
@@ -11,9 +13,10 @@ public class About extends EditorAction implements DialogFinisher {
 
   @Override
   public void finishDialog (DialogHelper helper) {
-    helper.setText(R.id.about_version_number, R.string.NBP_Editor_version_name);
+    helper.setText(R.id.about_package_version, R.string.NBP_Editor_version_name);
     helper.setText(R.id.about_build_time, R.string.NBP_Editor_build_time);
     helper.setText(R.id.about_source_revision, R.string.NBP_Editor_source_revision);
+    helper.setText(R.id.about_liblouis_version, Louis.getVersion());
     helper.setTextFromAsset(R.id.about_copyright, "copyright");
   }
 
