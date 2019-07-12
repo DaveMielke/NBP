1fcb22959c48db2ca8e0b380618e70a6e2d09346
Dave Mielke <dave@mielke.cc>
2019-07-12 12:47:53 -0400

    Change the editor's default braille mode from text to cells. (dm)

--- editor/src/org/nbp/editor/ApplicationDefaults.java
+++ editor/src/org/nbp/editor/ApplicationDefaults.java
@@ -7,7 +7,7 @@ public abstract class ApplicationDefaults {
   public final static boolean PROTECT_TEXT = false;
   public final static int SIZE_LIMIT = 150000;
 
-  public final static BrailleMode BRAILLE_MODE = BrailleMode.TEXT;
+  public final static BrailleMode BRAILLE_MODE = BrailleMode.CELLS;
   public final static BrailleCode BRAILLE_CODE = BrailleCode.UEB;
 
   public final static String AUTHOR_NAME = "";
