b5f5ec21b46a0856eee2bac9d08b0becf24b7538
Dave Mielke <dave@mielke.cc>
2019-09-16 13:24:06 -0400

    Implement clicking on a word definition within a list. (dm)

--- b2g/ui/src/org/nbp/b2g/ui/actions/ShowDefinition.java
+++ b2g/ui/src/org/nbp/b2g/ui/actions/ShowDefinition.java
@@ -28,39 +28,52 @@ public class ShowDefinition extends CursorKeyAction {
 
     new DefineCommand(text.subSequence(from, to).toString()) {
       @Override
-      protected void handleResult (DefinitionList definitions) {
+      protected void handleResult (final DefinitionList definitions) {
         int size = definitions.size();
         String text = null;
         int first = 0;
+        PopupClickHandler handler = null;
 
         if (size == 0) {
         } else if (size == 1) {
           text = definitions.get(0).getDefinitionText();
         } else {
-          StringBuilder sb = new StringBuilder();
+          {
+            StringBuilder sb = new StringBuilder();
 
-          sb.append(getString(R.string.popup_select_definition));
-          first = 1;
+            sb.append(getString(R.string.popup_select_definition));
+            first = 1;
 
-          for (int index=0; index<size; index+=1) {
-            DefinitionEntry definition = definitions.get(index);
+            for (int index=0; index<size; index+=1) {
+              DefinitionEntry definition = definitions.get(index);
 
-            sb.append('\n');
-            sb.append(index+1);
+              sb.append('\n');
+              sb.append(index+1);
 
-            sb.append(": ");
-            sb.append(definition.getMatchedWord());
+              sb.append(": ");
+              sb.append(definition.getMatchedWord());
 
-            sb.append(": ");
-            sb.append(definition.getDatabaseDescription());
+              sb.append(": ");
+              sb.append(definition.getDatabaseDescription());
+            }
+
+            text = sb.toString();
           }
 
-          text = sb.toString();
+          handler = new PopupClickHandler() {
+            @Override
+            public boolean handleClick (int index) {
+              PopupEndpoint endpoint = new PopupEndpoint();
+              endpoint.set(definitions.get(index).getDefinitionText(), 0, null);
+              Endpoints.setCurrentEndpoint(endpoint);
+              return true;
+            }
+          };
         }
 
         if (text != null) {
           PopupEndpoint endpoint = new PopupEndpoint();
-          endpoint.set(text, first, null);
+          endpoint.set(text, first, handler);
           Endpoints.setCurrentEndpoint(endpoint);
         }
       }
