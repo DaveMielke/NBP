56e1060a2e40e245ffa65de3873a32ee544a037e
Dave Mielke <dave@mielke.cc>
2021-05-29 18:42:49 -0400

    The editor now needs to use InternalTranslatorIdentifier.

--- Gradle/editor/src/main/java/org/nbp/editor/BrailleCode.java
+++ Gradle/editor/src/main/java/org/nbp/editor/BrailleCode.java
@@ -2,12 +2,13 @@ package org.nbp.editor;
 
 import org.liblouis.Translator;
 import org.liblouis.TranslatorIdentifier;
+import org.liblouis.InternalTranslatorIdentifier;
 
 public enum BrailleCode {
-  UEB(TranslatorIdentifier.EN_UEB_G2),
-  EBAE(TranslatorIdentifier.EN_US_G2),
-  EL(TranslatorIdentifier.EL),
-  GRC_INTL_EN(TranslatorIdentifier.GRC_INTL_EN),
+  UEB(InternalTranslatorIdentifier.EN_UEB_G2),
+  EBAE(InternalTranslatorIdentifier.EN_US_G2),
+  EL(InternalTranslatorIdentifier.EL),
+  GRC_INTL_EN(InternalTranslatorIdentifier.GRC_INTL_EN),
   ; // end of enumeration
 
   private final TranslatorIdentifier translatorIdentifier;
